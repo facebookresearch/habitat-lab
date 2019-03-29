ba2f4932c6c0e18a72e479fa8d7983fb4b9a4e31
Daniel Caleb Gordon <danielgordon10@gmail.com>
2019-03-28 18:39:32 -0700

    added comments

--- habitat/utils/visualizations/utils.py
+++ habitat/utils/visualizations/utils.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 
 import numpy as np
 import imageio
@@ -90,12 +90,37 @@ def paste_overlapping_image(
     return background
 
 
-def images_to_video(images, output_dir, video_name, fps=10, quality=5):
+def images_to_video(
+    images: List[np.ndarray],
+    output_dir: str,
+    video_name: str,
+    fps: int = 10,
+    quality: Optional[float] = 5,
+    **kwargs
+):
+    """Calls imageio to run FFMPEG on a list of images. For more info on
+        parameters, see
+        https://imageio.readthedocs.io/en/stable/format_ffmpeg.html
+    Args:
+        images: The list of images. Images should be HxWx3 in RGB order.
+        output_dir: The folder to put the video in.
+        video_name: The navme for the video.
+        fps: Frames per second for the video. Not all values work with FFMPEG,
+            use at your own risk.
+        quality: Default is 5. Uses variable bit rate. Highest quality is 10,
+            lowest is 0.  Set to None to prevent variable bitrate flags to
+            FFMPEG so you can manually specify them using output_params instead.
+            Specifying a fixed bitrate using ‘bitrate’ disables this parameter.
+    """
+    assert 0 <= quality <= 10
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     video_name = video_name.replace(" ", "_").replace("\n", "_") + ".mp4"
     writer = imageio.get_writer(
-        os.path.join(output_dir, video_name), fps=fps, quality=quality
+        os.path.join(output_dir, video_name),
+        fps=fps,
+        quality=quality,
+        **kwargs
     )
     for im in tqdm.tqdm(images):
         writer.append_data(im)
