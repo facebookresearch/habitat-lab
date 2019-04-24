b0779a49c9312bf9fb1b921799dca68aa7327deb
Andrii Tytarenko <titarenkoan@gmail.com>
2019-04-24 19:57:20 +0300

    Fixed render function in vector environment

--- test/test_habitat_env.py
+++ test/test_habitat_env.py
@@ -211,6 +211,7 @@ def test_rl_vectorized_envs():
         assert len(rewards) == num_envs
         assert len(dones) == num_envs
         assert len(infos) == num_envs
+        assert envs.render(mode='rgb_array')
         if (i + 1) % configs[0].ENVIRONMENT.MAX_EPISODE_STEPS == 0:
             assert all(dones), "dones should be true after max_episode steps"
 
