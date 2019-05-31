c0c2b91a678cf78025374f84225d8945d4ed07a7
JasonJiazhiZhang <jiazhiz@andrew.cmu.edu>
2019-05-31 08:52:52 -0700

    Different approach for install option

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@
 
 import os.path
 import sys
-
+import glob
 import setuptools
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "habitat"))
 from version import VERSION  # noqa
@@ -27,19 +29,51 @@ LONG_DESCRIPTION = readme
 AUTHOR = "Facebook AI Research"
 LICENSE = license
 REQUIREMENTS = (reqs.strip().split("\n"),)
-EXTRA_REQUIREMENT = {
-    "baseline": ["torch>=1.0.0"],
-    "rl_baseline": ["torch>=1.0.0"],
-    "slam_baseline": [],
-}
+DEFAULT_EXCLUSION = ["test", "examples"]
+FULL_REQUIREMENTS = []
+for file_name in glob.glob("**/*requirements.txt", recursive=True):
+    with open(file_name) as f:
+        reqs = f.read()
+        FULL_REQUIREMENTS.extend(reqs.strip().split("\n"))
+
+
+class OptionedCommand(object):
+    user_options = [
+        ("include_baseline", None, "include habitat_baselines in installation")
+    ]
+
+    def initialize_options(self):
+        super().initialize_options(self)
+        self.include_baseline = None
+
+    def finalize_options(self):
+        super().finalize_options(self)
+
+    def run(self):
+        if not self.include_baseline:
+            DEFAULT_EXCLUSION.append("baselines")
+        else:
+            REQUIREMENTS[:] = FULL_REQUIREMENTS
+        super().run(self)
+
+
+class InstallCommand(OptionedCommand, install):
+    user_options = (
+        getattr(install, "user_options", []) + OptionedCommand.user_options
+    )
+
+
+class DevelopCommand(OptionedCommand, develop):
+    user_options = (
+        getattr(develop, "user_options", []) + OptionedCommand.user_options
+    )
+
 
 if __name__ == "__main__":
     setuptools.setup(
         name=DISTNAME,
         install_requires=REQUIREMENTS,
-        packages=setuptools.find_packages(
-            exclude=["test", "examples", "habitat_baselines"]
-        ),
+        packages=setuptools.find_packages(exclude=DEFAULT_EXCLUSION),
         version=VERSION,
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
@@ -47,6 +81,6 @@ if __name__ == "__main__":
         license=LICENSE,
         setup_requires=["pytest-runner"],
         tests_require=["pytest"],
-        extras_require=EXTRA_REQUIREMENT,
         include_package_data=True,
+        cmdclass={"install": InstallCommand, "develop": DevelopCommand},
     )
