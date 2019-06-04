ee109f84892ad17ca08ff65c8c96557b9f0ce881
Erik Wijmans <etw@gatech.edu>
2019-06-03 22:41:03 -0700

    Try to make slightly less gross

--- habitat/sims/registration.py
+++ habitat/sims/registration.py
@@ -9,22 +9,23 @@ from habitat.core.registry import registry
 from habitat.core.simulator import Simulator
 
 
-try:
-    import habitat_sim
+def _try_register_habitat_sim():
+    try:
+        import habitat_sim
 
-    has_habitat_sim = True
-except ImportError as e:
-    has_habitat_sim = False
-    habitat_sim_import_error = e
+        has_habitat_sim = True
+    except ImportError as e:
+        has_habitat_sim = False
+        habitat_sim_import_error = e
 
-if has_habitat_sim:
-    from habitat.sims.habitat_simulator import HabitatSim
-else:
+    if has_habitat_sim:
+        from habitat.sims.habitat_simulator import HabitatSim
+    else:
 
-    @registry.register_simulator(name="Sim-v0")
-    class HabitatSimImportError(Simulator):
-        def __init__(self, *args, **kwargs):
-            raise habitat_sim_import_error
+        @registry.register_simulator(name="Sim-v0")
+        class HabitatSimImportError(Simulator):
+            def __init__(self, *args, **kwargs):
+                raise habitat_sim_import_error
 
 
 def make_sim(id_sim, **kwargs):
@@ -34,3 +35,6 @@ def make_sim(id_sim, **kwargs):
         id_sim
     )
     return _sim(**kwargs)
+
+
+_try_register_habitat_sim()
