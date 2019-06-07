511e899155e37045709247feea0592143187488b
Bhavana Jain <bhavanajain@fb.com>
2019-06-07 11:40:33 -0700

    fix linting

--- habitat/datasets/pointnav/pointnav_dataset.py
+++ habitat/datasets/pointnav/pointnav_dataset.py
@@ -34,7 +34,9 @@ class PointNavDatasetV1(Dataset):
 
     @staticmethod
     def check_config_paths_exist(config: Config) -> bool:
-        return (os.path.exists(config.POINTNAVV1.DATA_PATH.format(split=config.SPLIT)) and os.path.exists(config.SCENES_DIR))
+        return os.path.exists(
+            config.POINTNAVV1.DATA_PATH.format(split=config.SPLIT)
+        ) and os.path.exists(config.SCENES_DIR)
 
     @staticmethod
     def get_scenes_to_load(config: Config) -> List[str]:
