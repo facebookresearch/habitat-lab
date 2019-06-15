af32b4c65b4429b24479aa1d54c123d6c6fe9bce
Ricardo Cuenca <ricardocuenk@gmail.com>
2019-06-15 02:03:47 -0500

    Fix docs typo: step() -> act() (#128)
    Fix docs typo: step() -> act()

--- habitat/core/agent.py
+++ habitat/core/agent.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 r"""Base implementation of agent inside habitat. To build agents inside habitat 
-the user should subclass ``habitat.Agent`` and implement the ``step()``
+the user should subclass ``habitat.Agent`` and implement the ``act()``
 and ``reset()`` methods.
 """
 
