19bcd59a10941b2b82375b141c8a30a08d173fa1
erikwijmans <ewijmans2@gmail.com>
2019-09-03 14:39:49 -0700

    isort

--- test/test_habitat_env.py
+++ test/test_habitat_env.py
@@ -4,8 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import multiprocessing as mp
 import itertools
+import multiprocessing as mp
 import os
 
 import numpy as np
