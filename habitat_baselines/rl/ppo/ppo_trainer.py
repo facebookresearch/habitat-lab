f2637d271d0cb9380e4a64433b6535f954c11ec7
Erik Wijmans <etw@gatech.edu>
2019-09-12 11:21:52 -0400

    The indexing for a pause is suppose to happen on the batch dim

--- habitat_baselines/rl/ppo/ppo_trainer.py
+++ habitat_baselines/rl/ppo/ppo_trainer.py
@@ -515,7 +515,7 @@ class PPOTrainer(BaseRLTrainer):
 
                 # indexing along the batch dimensions
                 test_recurrent_hidden_states = test_recurrent_hidden_states[
-                    state_index
+                    :, state_index
                 ]
                 not_done_masks = not_done_masks[state_index]
                 current_episode_reward = current_episode_reward[state_index]
