762fc8366d573d9e979f3b750a1c33867b13a432
Aaron Gokaslan <agokaslan@fb.com>
2020-08-21 14:52:49 -0700

    More __init__ repair

--- habitat_baselines/rl/ddppo/policy/__init__.py
+++ habitat_baselines/rl/ddppo/policy/__init__.py
@@ -2,4 +2,6 @@
 
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# LICENSE file in the root directory of this source tree
+
+from .resnet_policy import PointNavResNetPolicy  # noqa: F401.
