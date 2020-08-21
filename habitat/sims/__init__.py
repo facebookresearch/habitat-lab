275096111bb11c77b851f30ec4e62cdcd71ac2e0
Aaron Gokaslan <agokaslan@fb.com>
2020-08-21 15:04:34 -0700

    Fix two inits

--- habitat/sims/__init__.py
+++ habitat/sims/__init__.py
@@ -3,4 +3,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from habitat.datasets.registration import make_dataseti  # noqa: F401
+
+from habitat.sims.registration import make_sim  # noqa: F401
