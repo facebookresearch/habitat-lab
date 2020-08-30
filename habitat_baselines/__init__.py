7928e29fe6b19102ab42db17d0e169e4f59cc21c
Mukul Khanna <mukul18khanna@gmail.com>
2020-08-30 14:45:12 +0000

    Updated baselines, utils __init__ files

--- habitat_baselines/__init__.py
+++ habitat_baselines/__init__.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from habitat_baselines.common.base_il_trainer import BaseILTrainer
 from habitat_baselines.common.base_trainer import BaseRLTrainer, BaseTrainer
 from habitat_baselines.il.trainers.eqa_cnn_pretrain_trainer import (
     EQACNNPretrainTrainer,
@@ -14,6 +15,7 @@ from habitat_baselines.rl.ppo.ppo_trainer import PPOTrainer, RolloutStorage
 __all__ = [
     "BaseTrainer",
     "BaseRLTrainer",
+    "BaseILTrainer",
     "PPOTrainer",
     "RolloutStorage",
     "EQACNNPretrainTrainer",
