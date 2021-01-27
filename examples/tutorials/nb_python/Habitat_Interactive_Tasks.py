575240c2e11f701d270d1c4dbefaa0b9b7268872
Vivian Auduong <auduong@fb.com>
2021-01-27 14:04:29 -0600

    Update habitat-lab with habitat-sim changes

--- examples/tutorials/nb_python/Habitat_Interactive_Tasks.py
+++ examples/tutorials/nb_python/Habitat_Interactive_Tasks.py
@@ -705,7 +705,7 @@ def raycast(sim, sensor_name, crosshair_pos=(128, 128), max_distance=2.0):
     """
     render_camera = sim._sensors[
         sensor_name
-    ]._sensor_object.get_render_camera()
+    ]._sensor_object.render_camera
     center_ray = render_camera.unproject(mn.Vector2i(crosshair_pos))
 
     raycast_results = sim.cast_ray(center_ray, max_distance=max_distance)
