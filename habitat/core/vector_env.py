b5e62048bbe3f749e6a753e2c50d7a111e4d094d
Aaron Gokaslan <agokaslan@fb.com>
2021-02-04 17:57:26 -0500

    [PERFORMANCE] Pickle5 ConnectionWrapper for VectorEnv at least (2-5% speed up) (#582)
    PEP 574 introduced a zero-copy pickling method that makes the pickling of Numpy arrays significantly faster. However, this feature is only supported on the latest Pickle protocol (5) which is not used by the default for multiprocessing (or even torch.multiprocessing, although it is often used in wrappers like Queues).
    I ported and slightly modified the torch Connection wrapper code to habitat-lab and improved it by adding a pickle5 backport. The backport port brings this functionality back to Python<3.8 After using this PR, it should remove a copy from NumPy arrays.
    
    Furthermore, this should not affect the pickling of any other parts of the code stack or any other multiprocessing code.
    
    This speed up is likely to be a lot more on higher resolution outputs from habitat-sim as well as the sensor numbers increase.
    
    This change should be fully backwards compatible and speed things up across the board without any code changes for the end user.

--- habitat/core/vector_env.py
+++ habitat/core/vector_env.py
@@ -35,6 +35,7 @@ from habitat.core.env import Env, RLEnv
 from habitat.core.logging import logger
 from habitat.core.utils import tile_images
 from habitat.utils import profiling_wrapper
+from habitat.utils.pickle5_multiprocessing import ConnectionWrapper
 
 try:
     # Use torch.multiprocessing if we can.
@@ -46,6 +47,7 @@ try:
 except ImportError:
     torch = None
     import multiprocessing as mp  # type:ignore
+
 
 STEP_COMMAND = "step"
 RESET_COMMAND = "reset"
@@ -300,7 +302,10 @@ class VectorEnv:
         workers_ignore_signals: bool = False,
     ) -> Tuple[List[_ReadWrapper], List[_WriteWrapper]]:
         parent_connections, worker_connections = zip(
-            *[self._mp_ctx.Pipe(duplex=True) for _ in range(self._num_envs)]
+            *[
+                [ConnectionWrapper(c) for c in self._mp_ctx.Pipe(duplex=True)]
+                for _ in range(self._num_envs)
+            ]
         )
         self._workers = []
         for worker_conn, parent_conn, env_args in zip(
