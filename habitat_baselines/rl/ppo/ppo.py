1e057dad030cbbb4c9ab42d63ca39b34f97407c9
Erik Wijmans <etw@gatech.edu>
2021-02-08 18:05:41 -0500

    Fix DDP Reduction in PyTorch 1.7 when the model has unused parameters (#586)
    * Hacky thing to interface with ddp.forward
    
    * State guard
    
    * Add unit test and comment in readme about it.

--- habitat_baselines/rl/ppo/ppo.py
+++ habitat_baselines/rl/ppo/ppo.py
@@ -88,7 +88,7 @@ class PPO(nn.Module):
                     action_log_probs,
                     dist_entropy,
                     _,
-                ) = self.actor_critic.evaluate_actions(
+                ) = self._evaluate_actions(
                     batch["observations"],
                     batch["recurrent_hidden_states"],
                     batch["prev_actions"],
@@ -151,6 +151,16 @@ class PPO(nn.Module):
         dist_entropy_epoch /= num_updates
 
         return value_loss_epoch, action_loss_epoch, dist_entropy_epoch
+
+    def _evaluate_actions(
+        self, observations, rnn_hidden_states, prev_actions, masks, action
+    ):
+        r"""Internal method that calls Policy.evaluate_actions.  This is used instead of calling
+        that directly so that that call can be overrided with inheritence
+        """
+        return self.actor_critic.evaluate_actions(
+            observations, rnn_hidden_states, prev_actions, masks, action
+        )
 
     def before_backward(self, loss: Tensor) -> None:
         pass
