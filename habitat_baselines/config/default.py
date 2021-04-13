dc8f2a73201e700e3a28e634c75b82fe38f35f55
Naoki Harrison Yokoyama <nyokoyama3@irona.cc.gatech.edu>
2021-04-13 13:45:03 -0400

    removed unnecessary lines from default.py

--- habitat_baselines/config/default.py
+++ habitat_baselines/config/default.py
@@ -132,8 +132,6 @@ _C.RL.PPO.tau = 0.95
 _C.RL.PPO.reward_window_size = 50
 _C.RL.PPO.use_normalized_advantage = False
 _C.RL.PPO.hidden_size = 512
-# _C.RL.PPO.action_distribution_type = 'categorical'
-_C.RL.PPO.action_distribution_type = 'gaussian'
 # Use double buffered sampling, typically helps
 # when environment time is similar or large than
 # policy inference time during rollout generation
