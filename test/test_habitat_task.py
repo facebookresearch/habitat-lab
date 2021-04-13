fixed test for rotation checking

test/test_habitat_task.py
@@ -36,11 +36,13 @@ def test_task_actions():
         env.step(action)
         agent_state = env.sim.get_agent_state()
         assert np.allclose(
-            np.array(TELEPORT_POSITION, dtype=np.float32), agent_state.position
+            np.array(TELEPORT_POSITION, dtype=np.float32),
+            agent_state.position,
         ), "mismatch in position after teleport"
         assert np.allclose(
             np.array(TELEPORT_ROTATION, dtype=np.float32),
             np.array([*agent_state.rotation.imag, agent_state.rotation.real]),
+            atol=1e-5,
         ), "mismatch in rotation after teleport"
         env.step("TURN_RIGHT")
 