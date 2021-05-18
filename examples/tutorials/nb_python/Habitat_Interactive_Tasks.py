c7a90b2ab3dac07065e4e02fd9395f11f21a990c
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-05-18 17:18:35 -0400

    Test Pyrobot

--- examples/tutorials/nb_python/Habitat_Interactive_Tasks.py
+++ examples/tutorials/nb_python/Habitat_Interactive_Tasks.py
@@ -14,7 +14,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.11.1
+#       jupytext_version: 1.11.2
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
