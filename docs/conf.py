fb4bc37dc028338da0f2b58f59cc0ad61fa8e9e2
Oleksandr Maksymets <maksymets@gmail.com>
2021-06-29 16:07:46 -0700

    Switch tensorflow to safe versions, fixed headers

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import os
 import sys
 
