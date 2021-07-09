d8a3b981ebef4672b5e80162dba89993aad6b513
Erik Wijmans <etw@gatech.edu>
2021-07-09 12:01:24 -0400

    That should do it

--- habitat/datasets/rearrange/rearrange_dataset.py
+++ habitat/datasets/rearrange/rearrange_dataset.py
@@ -12,6 +12,7 @@ import attr
 import habitat_sim.utils.datasets_download as data_downloader
 from habitat.config import Config
 from habitat.core.dataset import Episode
+from habitat.core.logging import logger
 from habitat.core.registry import registry
 from habitat.core.utils import DatasetFloatJSONEncoder
 from habitat.datasets.pointnav.pointnav_dataset import PointNavDatasetV1
@@ -46,11 +47,14 @@ class RearrangeDatasetV0(PointNavDatasetV1):
         self.config = config
 
         if config and not self.check_config_paths_exist(config):
-            print(
+            logger.info(
                 "Rearrange task assets are not downloaded locally, downloading and extracting now..."
             )
-            data_downloader.main(["--uids", "rearrange_task_assets"])
-            print("Downloaded and extracted the data.")
+            data_downloader.main(
+                ["--uids", "rearrange_task_assets", "--no-replace"]
+            )
+            logger.info("Downloaded and extracted the data.")
+
         check_and_gen_physics_config()
 
         super().__init__(config)
