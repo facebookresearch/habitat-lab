5dc2f8493ece5a39d02106d392a502557dec4b6e
Eric Undersander <eundersander@gmail.com>
2021-10-30 14:37:00 -0700

    add some helper shell scripts

---
+++ gala_kinematic_train.sh
@@ -0,0 +1,1 @@
+python ./habitat_baselines/run.py --exp-config habitat_baselines/config/rearrange/gala_kinematic.yaml --run-type train
---
+++ pyspy_train_gala_kinematic.sh
@@ -0,0 +1,1 @@
+py-spy record --idle --function --native --subprocesses --rate 50 --output pyspy_$1.speedscope --format speedscope -- python ./habitat_baselines/run.py --exp-config habitat_baselines/config/rearrange/gala_kinematic.yaml --run-type train
