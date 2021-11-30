5a4d7b7ccaa62a8099c49919b83399c5ac170691
ASzot <me@andrewszot.com>
2021-11-30 10:31:31 -0800

    Revert "linting fixes"
    This reverts commit f38de79b3725b1aca217ffad4cdd7c27d7b262d2.

--- habitat_baselines/rl/ppo/ppo_trainer.py
+++ habitat_baselines/rl/ppo/ppo_trainer.py
@@ -50,6 +50,7 @@ from habitat_baselines.rl.ppo import PPO
 from habitat_baselines.rl.ppo.policy import Policy
 from habitat_baselines.utils.common import (
     ObservationBatchingCache,
+    action_array_to_dict,
     action_to_velocity_control,
     batch_obs,
     generate_video,
