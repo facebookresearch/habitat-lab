02d67ef4ec61e01dcb3673e439edc56f1fcfac0e
ASzot <me@andrewszot.com>
2022-03-08 10:50:27 -0800

    Documented dynamic task utils

--- habitat/tasks/rearrange/multi_task/dynamic_task_utils.py
+++ habitat/tasks/rearrange/multi_task/dynamic_task_utils.py
@@ -6,30 +6,43 @@
 
 import copy
 import os.path as osp
+from typing import Any, Dict
 
 import habitat
+from habitat import Config
+from habitat.core.dataset import Episode
 from habitat.core.registry import registry
+from habitat.datasets.rearrange.rearrange_dataset import RearrangeDatasetV0
+from habitat.tasks.rearrange.rearrange_task import RearrangeTask
 
 TASK_CONFIGS_DIR = "configs/tasks/rearrange/"
 
 
 def load_task_object(
-    task,
-    task_def,
-    cur_config,
-    cur_env,
-    cur_dataset,
-    should_super_reset,
-    task_kwargs,
-    episode,
-):
-    task_cls = registry.get_task(task)
+    task_cls_name: str,
+    task_config_path: str,
+    cur_config: Config,
+    cur_env: RearrangeTask,
+    cur_dataset: RearrangeDatasetV0,
+    should_super_reset: bool,
+    task_kwargs: Dict[str, Any],
+    episode: Episode,
+) -> RearrangeTask:
+    """
+    Loads a task. Used when a task needs to be simulated within another task. For example, this is used to get the starting state of another task as a navigation goal in the Habitat 2.0 navigation task. The loaded task uses the information and dataset from the main task (which is also passed into this function).
+
+    :param task_cls_name: The name of the task to load.
+    :param task_config_path: The path to the config for the task to load.
+    :param cur_config: The config for the main task.
+    :param cur_env: The main task.
+    """
+    task_cls = registry.get_task(task_cls_name)
 
     config = copy.copy(cur_config)
     config.defrost()
-    if task_def is not None:
+    if task_config_path is not None:
         task_config = habitat.get_config(
-            osp.join(TASK_CONFIGS_DIR, task_def + ".yaml")
+            osp.join(TASK_CONFIGS_DIR, task_config_path + ".yaml")
         )
         config.merge_from_other_cfg(task_config.TASK)
     config.freeze()
