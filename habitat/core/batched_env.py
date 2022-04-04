b49d2bf9b258e238ca3973d6a191257156cd3127
vincentpierre <vincentpierre@users.noreply.github.com>
2022-04-04 10:32:46 -0700

    removing useless import

--- habitat/core/batched_env.py
+++ habitat/core/batched_env.py
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 from collections import OrderedDict
-from turtle import shape
 from typing import Any, Dict, List, Optional
 
 import numpy as np
