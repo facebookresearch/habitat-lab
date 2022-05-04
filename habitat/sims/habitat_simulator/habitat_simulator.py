903fea9524a02fa8bf6d43387e02d1a1aa31b7ba
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-05-04 16:01:30 -0400

    Fix mypy typing error

--- habitat/sims/habitat_simulator/habitat_simulator.py
+++ habitat/sims/habitat_simulator/habitat_simulator.py
@@ -234,7 +234,9 @@ class HabitatSimFisheyeSemanticSensor(HabitatSimSemanticSensor):
     _get_default_spec = habitat_sim.FisheyeSensorDoubleSphereSpec
 
 
-def check_sim_obs(obs: Optional[np.ndarray], sensor: Sensor) -> None:
+def check_sim_obs(
+    obs: Union[np.ndarray, "Tensor", None], sensor: Sensor
+) -> None:
     assert obs is not None, (
         "Observation corresponding to {} not present in "
         "simulator's observations".format(sensor.uuid)
