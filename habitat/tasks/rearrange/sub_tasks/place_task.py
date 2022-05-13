e22554d90f0e1b1189b98c729a6368d7827afd17
ASzot <me@andrewszot.com>
2022-05-13 12:58:30 -0700

    Fixed bug with robot holding object already on place skill reset. Added missing object in success condition.

--- habitat/tasks/rearrange/sub_tasks/place_task.py
+++ habitat/tasks/rearrange/sub_tasks/place_task.py
@@ -28,6 +28,9 @@ class RearrangePlaceTaskV1(RearrangePickTaskV1):
 
     def reset(self, episode: Episode):
         sim = self._sim
+        # Remove whatever the agent is currently holding.
+        sim.grasp_mgr.desnap(force=True)
+
         super().reset(episode, fetch_observations=False)
 
         abs_obj_idx = sim.scene_obj_ids[self.abs_targ_idx]
