ae23dec3ccd795b3c3e8073cc0590eb620de2e75
Laikh Tewari <laikh@fb.com>
2022-07-07 15:04:42 -0700

    Init with input params

--- habitat/robots/static_manipulator.py
+++ habitat/robots/static_manipulator.py
@@ -4,7 +4,50 @@
 
 @attr.s(auto_attribs=True, slots=True)
 class StaticManipulatorParams:
-    pass 
+    """Data to configure a static manipulator.
+    :property arm_joints: The joint ids of the arm joints.
+    :property gripper_joints: The habitat sim joint ids of any grippers.
+    :property arm_init_params: The starting joint angles of the arm. If None,
+        resets to 0.
+    :property gripper_init_params: The starting joint positions of the gripper. If None,
+        resets to 0.
+    :property ee_offset: The 3D offset from the end-effector link to the true
+        end-effector position.
+    :property ee_link: The Habitat Sim link ID of the end-effector.
+    :property ee_constraint: A (2, N) shaped array specifying the upper and
+        lower limits for each end-effector joint where N is the arm DOF.
+    :property cameras: The cameras and where they should go. The key is the 
+        prefix to match in the sensor names. For example, a key of `"robot_head"`
+        will match sensors `"robot_head_rgb"` and `"robot_head_depth"`
+    :property gripper_closed_state: All gripper joints must achieve this
+        state for the gripper to be considered closed.
+    :property gripper_open_state: All gripper joints must achieve this
+        state for the gripper to be considered open.
+    :property gripper_state_eps: Error margin for detecting whether gripper is closed.
+    :property arm_mtr_pos_gain: The position gain of the arm motor.
+    :property arm_mtr_vel_gain: The velocity gain of the arm motor.
+    :property arm_mtr_max_impulse: The maximum impulse of the arm motor.
+    """
+
+    arm_joints: List[int]
+    gripper_joints: List[int]
+
+    arm_init_params: Optional[np.ndarray]
+    gripper_init_params: Optional[np.ndarray]
+
+    ee_offset: mn.Vector3
+    ee_link: int
+    ee_constraint: np.ndarray
+
+    cameras: Dict[str, RobotCameraParams] # TODO: Are cameras necessary?
+
+    gripper_closed_state: np.ndarray
+    gripper_open_state: np.ndarray
+    gripper_state_eps: float
+
+    arm_mtr_pos_gain: float
+    arm_mtr_vel_gain: float
+    arm_mtr_max_impulse: float
 
 class StaticManipulator(RobotInterface):
     """Robot with a fixed base and controllable arm."""
@@ -12,10 +55,42 @@ class StaticManipulator(RobotInterface):
     def __init__(
         self,
         params: StaticManipulatorParams,
+        urdf_path: str,
+        sim: Simulator,
     ):
         r"""Constructor
         """
         super().__init__()
+        self.urdf_path = urdf_path
+        self.params = params
+        self._sim = sim
+        self.sim_obj = None
+
+        #TODO: Are cameras needed?
+        self._cameras = defaultdict(list)
+        for camera_prefix in self.params.cameras:
+            for sensor_name in self._sim._sensors:
+                if sensor_name.startswith(camera_prefix):
+                    self._cameras[camera_prefix].append(sensor_name)
+
+        # NOTE: the follow members cache static info for improved efficiency over querying the API
+        # maps joint ids to motor settings for convenience
+        self.joint_motors: Dict[int, Tuple[int, JointMotorSettings]] = {}
+        # maps joint ids to position index
+        self.joint_pos_indices: Dict[int, int] = {}
+        # maps joint ids to velocity index
+        self.joint_dof_indices: Dict[int, int] = {}
+        self.joint_limits: Tuple[np.ndarray, np.ndarray] = None
+
+        # defaults for optional params
+        if self.params.gripper_init_params is None:
+            self.params.gripper_init_params = np.zeros(
+                len(self.params.gripper_joints), dtype=np.float32
+            )
+        if self.params.arm_init_params is None:
+            self.params.arm_init_params = np.zeros(
+                len(self.params.arm_joints), dtype=np.float32
+            )
 
     def reconfigure(self) -> None:
         pass
