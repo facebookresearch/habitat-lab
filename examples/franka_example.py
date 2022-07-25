b2084e5f89e80a4162fd2175bc913c173732f706
Laikh Tewari <laikh@fb.com>
2022-07-25 13:49:10 -0700

    add no render option for pytest in franka example

--- examples/franka_example.py
+++ examples/franka_example.py
@@ -4,10 +4,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import argparse
+
 import cv2
 import numpy as np
 
 import habitat
+from habitat.core.logging import logger
 
 SENSOR_KEY = "rgb"
 
@@ -15,36 +18,41 @@ SENSOR_KEY = "rgb"
 def display_grayscale(image):
     img_bgr = np.repeat(image, 3, 2)
     cv2.imshow("Depth Sensor", img_bgr)
-    _ = cv2.waitKey(0)
+    cv2.waitKey(0)
 
 
 def display_rgb(image):
     img_bgr = image[..., ::-1]
     cv2.imshow("RGB", img_bgr)
-    _ = cv2.waitKey(0)
+    cv2.waitKey(0)
 
 
-def example():
+def example(args):
     # Note: Use with for the example testing, doesn't need to be like this on the README
 
     with habitat.Env(
         config=habitat.get_config("configs/tasks/franka_point.yaml")
     ) as env:
-        print("Environment creation successful")
+        logger.info("Environment creation successful")
         observations = env.reset()  # noqa: F841
-        display_rgb(observations[SENSOR_KEY])
+        if not args.no_render:
+            display_rgb(observations[SENSOR_KEY])
 
-        print("Agent acting inside environment.")
+        logger.info("Agent acting inside environment.")
         count_steps = 0
-        print(env.action_space)
+        logger.info(env.action_space)
         while not env.episode_over:
             action = env.action_space.sample()
-            print("Executing action:", action)
+            logger.info("Executing action:", action)
             observations = env.step(action)  # noqa: F841
-            display_rgb(observations[SENSOR_KEY])
+            if not args.no_render:
+                display_rgb(observations[SENSOR_KEY])
             count_steps += 1
-        print("Episode finished after {} steps.".format(count_steps))
+        logger.info("Episode finished after {} steps.".format(count_steps))
 
 
 if __name__ == "__main__":
-    example()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--no-render", action="store_true", default=False)
+    args = parser.parse_args()
+    example(args)
