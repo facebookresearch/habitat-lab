7d192f5009a41fef3e96a9bd351e899d30522fe4
vincentpierre <vincentpierre@users.noreply.github.com>
2022-08-01 17:10:47 -0700

    adding configs to pip package

--- setup.py
+++ setup.py
@@ -88,6 +88,7 @@ if __name__ == "__main__":
         name=DISTNAME,
         install_requires=REQUIREMENTS,
         packages=setuptools.find_packages(exclude=DEFAULT_EXCLUSION),
+        package_data={"habitat": ["config/**/*.yaml"]},
         version=VERSION,
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
