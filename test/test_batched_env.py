59be583641c0d210c3716447ef9197d0213119a4
Eric Undersander <eundersander@fb.com>
2022-08-04 10:16:00 -0400

    add test_benchmark

--- test/test_batched_env.py
+++ test/test_batched_env.py
@@ -5,9 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 
 import pickle
+import time
 
 import torch
 
+import habitat_sim
 from habitat.core.batched_env import BatchedEnv
 from habitat_baselines.config.default import get_config
 
@@ -40,7 +42,7 @@ def test_basic():
         batched_env.async_step(actions)
         outputs = batched_env.wait_step()
 
-        # temp
+        # convert tensors to lists
         batched_observations, _, _, _ = outputs
         for key in batched_observations.keys():
             value = batched_observations[key]
@@ -59,5 +61,39 @@ def test_basic():
     # batched_env.close()
 
 
+def test_benchmark():
+
+    print("build_type: ", habitat_sim.build_type)
+
+    exp_config = "habitat_baselines/config/rearrange/gala_kinematic_local.yaml"
+    opts = None
+    config = get_config(exp_config, opts)
+
+    batched_env = BatchedEnv(config)
+
+    _ = batched_env.reset()
+
+    torch.manual_seed(0)
+
+    t_recent = time.time()
+
+    for batch_step_idx in range(1000000):
+        actions = torch.rand(batched_env.num_envs, batched_env.action_dim)
+        batched_env.async_step(actions)
+        # todo: use outputs to ensure they don't get optimized away
+        _ = batched_env.wait_step()
+
+        log_interval = 100
+        if batch_step_idx != 0 and batch_step_idx % log_interval == 0:
+            t_curr = time.time()
+            num_recent_steps = log_interval * batched_env.num_envs
+            sps = num_recent_steps / (t_curr - t_recent)
+            t_recent = t_curr
+
+            print(
+                "batch_step_idx: {}, sps: {:.2f}".format(batch_step_idx, sps)
+            )
+
+
 if __name__ == "__main__":
-    test_basic()
+    test_benchmark()
