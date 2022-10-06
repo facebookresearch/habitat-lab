41b02edd5c0515a368d34fd066747148fe249eef
vincentpierre <vincentpierre@users.noreply.github.com>
2022-10-05 17:02:20 -0700

    added an example of mixing structure configs and yaml
    Run the following code to see how it is used :
    
    from hydra import compose, initialize
    from omegaconf import DictConfig, OmegaConf
    
    import habitat
    import hydra
    from hydra.core.config_store import ConfigStore
    from dataclasses import dataclass, field
    from omegaconf import DictConfig, OmegaConf, MISSING
    from typing import Any
    
    # NOTE: import required to register structured configs
    import habitat.config.default_structured_configs
    from habitat.config.default_structured_configs import DatasetConfigPlugin, register_my_plugin
    
    from dataclasses import dataclass, field
    from typing import Any, List, Optional
    
    from hydra.core.config_store import ConfigStore
    from omegaconf import MISSING
    
    cs = ConfigStore.instance()
    
    cs = ConfigStore.instance()
    
    register_my_hydra_plugin(DatasetConfigPlugin)
    
    @hydra.main(config_name="datasets/eqa/mp3d")
    def my_app(cfg : DictConfig) -> None:
        print(OmegaConf.to_yaml(cfg))
    
    if __name__ == "__main__":
        my_app()

--- habitat-lab/habitat/config/default_structured_configs.py
+++ habitat-lab/habitat/config/default_structured_configs.py
@@ -1087,3 +1087,20 @@ cs.store(
     name="turn_right",
     node=TurnRightActionConfig,
 )
+
+from hydra.core.config_search_path import ConfigSearchPath
+from hydra.core.plugins import Plugins
+from hydra.plugins.search_path_plugin import SearchPathPlugin
+
+
+class DatasetConfigPlugin(SearchPathPlugin):
+    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
+        search_path.append(
+            provider="habitat",
+            path="pkg://habitat/config/",
+        )
+
+
+def register_my_hydra_plugin(plugin) -> None:
+    """Hydra users should call this function before invoking @hydra.main"""
+    Plugins.instance().register(DatasetConfigPlugin)
