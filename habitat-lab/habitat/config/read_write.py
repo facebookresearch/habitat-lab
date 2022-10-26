6d5e434c0e0fde921e3837d7d7ba6f87f3cf89b3
Mino <minonakura@fb.com>
2022-10-26 14:49:35 -0700

    adding copyright block to files without them

--- habitat-lab/habitat/config/read_write.py
+++ habitat-lab/habitat/config/read_write.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from contextlib import contextmanager
 from typing import Generator, Union
 
