0b4b7161ba87863675666371d1864103d1fd5154
theophilee <theophile.gervet@gmail.com>
2022-11-14 10:03:59 -0500

    Move stuff

--- exp/theo/habitat_projects/tasks/object_navigation/agent/objectnav_agent_module.py
+++ exp/theo/habitat_projects/tasks/object_navigation/agent/objectnav_agent_module.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import time
 
-from agent.mapping.metric.semantic.semantic_map_module import SemanticMapModule
+from agent.mapping.dense.semantic.semantic_map_module import SemanticMapModule
 from agent.navigation_policy.object_navigation.objectnav_frontier_exploration_policy import (
     ObjectNavFrontierExplorationPolicy,
 )
