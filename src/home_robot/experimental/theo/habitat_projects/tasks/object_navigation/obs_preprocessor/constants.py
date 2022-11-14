95b123440428f22073fecf8023df2619a04f84c7
theophilee <theophile.gervet@gmail.com>
2022-11-14 11:56:54 -0500

    Move stuff

--- src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/obs_preprocessor/constants.py
+++ src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/obs_preprocessor/constants.py
@@ -1,4 +1,4 @@
-from home_robot.agent.perception.segmentation.coco_maskrcnn.coco_categories import (
+from home_robot.agent.perception.detection.coco_maskrcnn.coco_categories import (
     coco_categories_color_palette,
 )
 
