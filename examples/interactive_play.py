0ec33a6ef402dc179c9626cfc27a0c0fbdc011a3
vincentpierre <vincentpierre@users.noreply.github.com>
2022-11-17 15:21:28 -0800

    [Bugfix] Fixing --add-ik in terractive play

--- examples/interactive_play.py
+++ examples/interactive_play.py
@@ -628,7 +628,7 @@ if __name__ == "__main__":
                 raise ValueError(
                     "Action space does not have any arm control so incompatible with `--add-ik` option"
                 )
-            sim_config.ik_arm_urdf = (
+            sim_config.agent_0.ik_arm_urdf = (
                 "./data/robots/hab_fetch/robots/fetch_onlyarm.urdf"
             )
             task_config.actions.arm_action.arm_controller = "ArmEEAction"
