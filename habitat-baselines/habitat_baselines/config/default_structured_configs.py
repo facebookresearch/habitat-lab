3b0ec450f1eba4cb63d3096994a7108507a397ed
vincentpierre <vincentpierre@users.noreply.github.com>
2022-11-21 14:45:02 -0800

    adding some missing files

--- habitat-baselines/habitat_baselines/config/default_structured_configs.py
+++ habitat-baselines/habitat_baselines/config/default_structured_configs.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 import math
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Tuple
