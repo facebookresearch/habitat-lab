ce2ec5808997212ec1146c42311455ca8e68d43d
rpartsey <partsey2412@gmail.com>
2022-11-30 01:23:24 +0200

    added a dot :)

--- habitat-lab/habitat/config/default_structured_configs.py
+++ habitat-lab/habitat/config/default_structured_configs.py
@@ -965,7 +965,7 @@ class SimulatorConfig(HabitatBaseConfig):
     # agents_order specifies the order in which the agents
     # are stored on the habitat-sim side.
     # In other words, the order to return the observations and accept
-    # the actions when using the environment API
+    # the actions when using the environment API.
     # If the number of agents is greater than one,
     # then agents_order has to be set explicitly.
     agents_order: List[str] = MISSING
