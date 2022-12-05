de9e969b0706dfb8aab2a405bb549e4b1e4c5541
theophilee <theophile.gervet@gmail.com>
2022-12-05 09:45:40 -0500

    Extract image instance nav images

--- src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/extract_image_instances.py
+++ src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/extract_image_instances.py
@@ -9,9 +9,10 @@ from habitat.core.env import Env
 
 
 if __name__ == "__main__":
-    config = habitat.get_config(str((
+    config = habitat.get_config()
+    config.merge_from_file(str(
         Path(__file__).resolve().parent.parent / "configs/task/hm3d_imageinstancegoal_val.yaml"
-    )))
+    ))
     env = Env(config=config)
     obs = env.reset()
     scene_id = env.current_episode.scene_id.split("/")[-1].split(".")[0]
