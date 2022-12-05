a5016f94edd9d59543a568aab6d5c838f551525e
theophilee <theophile.gervet@gmail.com>
2022-12-05 08:54:56 -0500

    Extract image instance nav images

--- src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/extract_image_instances.py
+++ src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/extract_image_instances.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     episode_dir = "home_robot/experimental/theo/habitat_projects/datasets/episode_datasets/imageinstancegoal_hm3d/val/content/*"
     for scene_path in glob.glob(episode_dir):
         with gzip.open(scene_path) as f:
-            scene_data = json.loads(f.read())
-            print(scene_path.keys())
+            x = f.read()
+            print(type(x))
