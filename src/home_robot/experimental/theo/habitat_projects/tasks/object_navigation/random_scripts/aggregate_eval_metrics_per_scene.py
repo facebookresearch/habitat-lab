fe68b21b9427c38558191ff0596b3381f7e87057
theophilee <theophile.gervet@gmail.com>
2022-12-06 13:17:03 -0500

    aggregate eval metrics per scene

---
+++ src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/aggregate_eval_metrics_per_scene.py
@@ -0,0 +1,20 @@
+"""
+This script is intended to run from the "src" root:
+python home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/aggregate_eval_metrics_per_scene.py
+"""
+import json
+import numpy as np
+from pprint import pprint
+from collections import defaultdict
+
+
+if __name__ == "__main__":
+    results_path = "home_robot/experimental/theo/habitat_projects/tasks/object_navigation/datadump/results/eval_floorplanner/val_episode_results.json"
+    episode_results = json.load(open(results_path, "r"))
+    scene_results = defaultdict(lambda x: [])
+    for k, v in episode_results.items():
+        scene_id = "_".join(k.split("_")[:-1])
+        scene_results[scene_id].append(v["success"])
+    scene_results = dict(episode_results)
+    scene_results = {k: np.mean(v) for k, v in scene_results.items()}
+    pprint(scene_results, indent=4)
