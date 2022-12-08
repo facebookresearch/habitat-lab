43ab8d437fc1361d9336e7eaee1f59326c3a487a
theophilee <theophile.gervet@gmail.com>
2022-12-07 19:37:26 -0500

    Test Detic

--- src/home_robot/agent/perception/detection/detic/detic/modeling/utils.py
+++ src/home_robot/agent/perception/detection/detic/detic/modeling/utils.py
@@ -6,7 +6,8 @@ from torch.nn import functional as F
 from pathlib import Path
 
 def load_class_freq(
-    path=str(Path(__file__).resolve().parent / 'datasets/metadata/lvis_v1_train_cat_info.json'), freq_weight=1.0):
+        path=str(Path(__file__).resolve().parent.parent.parent / 'datasets/metadata/lvis_v1_train_cat_info.json'),
+        freq_weight=1.0):
     cat_info = json.load(open(path, 'r'))
     cat_info = torch.tensor(
         [c['image_count'] for c in sorted(cat_info, key=lambda x: x['id'])])
