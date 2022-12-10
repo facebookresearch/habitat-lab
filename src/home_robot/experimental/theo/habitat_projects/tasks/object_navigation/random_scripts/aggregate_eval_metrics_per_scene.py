3d00497c8567d0e73a130f3d338c84e197ab795b
theophilee <theophile.gervet@gmail.com>
2022-12-10 09:35:05 -0500

    Identify buggy scenes

--- src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/aggregate_eval_metrics_per_scene.py
+++ src/home_robot/experimental/theo/habitat_projects/tasks/object_navigation/random_scripts/aggregate_eval_metrics_per_scene.py
@@ -9,12 +9,16 @@ from collections import defaultdict
 
 
 if __name__ == "__main__":
-    results_path = "home_robot/experimental/theo/habitat_projects/tasks/object_navigation/datadump/results/eval_floorplanner/val_episode_results.json"
-    episode_results = json.load(open(results_path, "r"))
+    results_paths = [
+        "home_robot/experimental/theo/habitat_projects/tasks/object_navigation/datadump/results/eval_floorplanner/val_episode_results.json",
+        "home_robot/experimental/theo/habitat_projects/tasks/object_navigation/datadump/results/eval_floorplanner/train_episode_results.json"
+    ]
     scene_results = defaultdict(list)
-    for k, v in episode_results.items():
-        scene_id = "_".join(k.split("_")[:-1])
-        scene_results[scene_id].append(v["success"])
+    for path in results_paths:
+        episode_results = json.load(open(path, "r"))
+        for k, v in episode_results.items():
+            scene_id = "_".join(k.split("_")[:-1])
+            scene_results[scene_id].append(v["success"])
     scene_results = dict(scene_results)
     scene_results = {k: {"success": np.mean(v), "episodes": len(v)} for k, v in scene_results.items()}
     pprint(scene_results, indent=4)
