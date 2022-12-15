3083b92666a7efcfec6d036b0fcb43de1368bf95
aclegg3 <alexanderwclegg@gmail.com>
2022-12-15 15:55:08 -0800

    pre-commit cleanup

--- scripts/receptacle_annotation/blender_export_aabb_receptacles.py
+++ scripts/receptacle_annotation/blender_export_aabb_receptacles.py
@@ -14,18 +14,16 @@ import mathutils
 # NOTE: tested with Blender 3.x+
 # This script should be run from within Blender script interface.
 
-#Run this script to export all 
-
-
-#NOTE: modify this path to include desired output directory
+# NOTE: modify this path to include desired output directory
 output_filename = "receptacle_output.json"
-#the generic prefix marking an object as an aabb receptacle
+# the generic prefix marking an object as an aabb receptacle
 mesh_receptacle_id_string = "receptacle_aabb_"
 
-#transformation from Blender to Habitat coordinate system
+# transformation from Blender to Habitat coordinate system
 to_hab = mathutils.Quaternion((1.0, 0.0, 0.0), math.radians(-90.0))
-#the JSON config dict to fill
+# the JSON config dict to fill
 user_defined = {}
+
 
 def write_object_receptacles():
     """
@@ -34,24 +32,22 @@ def write_object_receptacles():
     """
     for obj in bpy.context.scene.objects:
         if "receptacle_" in obj.name:
-            receptacle_info = {}
-            receptacle_info["name"] = obj.name
+            receptacle_info = {
+                "name": obj.name,
+                # NOTE: hardcoded for now, set this yourself
+                "parent_object": "kitchen_island",
+                "parent_link": obj.parent.name.split("link_")[-1],
+                "position": list(obj.location),
+                # NOTE: need half-extents for the final size
+                "scale": list(obj.scale * 0.5),
+                # NOTE: default hardcoded value for now
+                "up": [0, 1, 0],
+            }
 
             # get top level parent
             # top_parent = obj.parent
             # while top_parent.parent is not None:
             #    top_parent = top_parent.parent
-            # NOTE: hardcoded for now
-            receptacle_info["parent_object"] = "kitchen_island"
-            receptacle_info["parent_link"] = obj.parent.name.split("link_")[-1]
-
-            receptacle_info["position"] = list(obj.location)
-
-            # NOTE: need half-extents for the final size
-            receptacle_info["scale"] = list(obj.scale * 0.5)
-
-            # NOTE: default hardcoded value for now
-            receptacle_info["up"] = [0, 1, 0]
 
             user_defined[obj.name] = receptacle_info
 
@@ -63,8 +59,7 @@ def write_global_receptacles():
     """
     for obj in bpy.context.scene.objects:
         if "receptacle_" in obj.name:
-            receptacle_info = {}
-            receptacle_info["name"] = obj.name
+            receptacle_info = {"name": obj.name}
 
             location = obj.location.copy()
             rotation = obj.rotation_quaternion.copy()
@@ -83,14 +78,15 @@ def write_global_receptacles():
 
             user_defined[obj.name] = receptacle_info
 
-#main
 
-#pick your mode:
+# main
+
+# pick your mode:
 write_global_receptacles()
-#write_object_receptacles()
+# write_object_receptacles()
 
-#write the metadata
-output_dir = output_filename[:-len(output_filename.split("/")[-1])]
+# write the metadata
+output_dir = output_filename[: -len(output_filename.split("/")[-1])]
 os.makedirs(output_dir, exist_ok=True)
 with open(output_filename, "w") as f:
     json.dump(user_defined, f, indent=4)
