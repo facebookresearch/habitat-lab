e53f0338c435f806ab8c88a556cce19340451870
Xavi Puig <xavierpuig@fb.com>
2023-01-03 10:54:11 -0500

    add human controller

---
+++ habitat-baselines/human_controllers/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from human_controllers.amass_human_controller import AmassHumanController
+
+__all__ = [
+    "AmassHumanController"
+]
