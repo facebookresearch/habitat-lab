a2752a99d8a0f3ae71d57cd04b6f634a0b6fa812
Sriram Yenamandra <yvsriram99@gmail.com>
2023-01-15 18:01:44 -0500

    add nav task definition

---
+++ habitat-lab/habitat/tasks/rearrange/sub_tasks/cat_nav_to_obj_task.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import numpy as np
+
+from habitat.core.registry import registry
+from habitat.tasks.rearrange.sub_tasks.nav_to_obj_task import DynNavRLEnv
+
+
+@registry.register_task(name="CatNavToObjTask-v0")
+class CatDynNavRLEnv(DynNavRLEnv):
+    def _generate_nav_to_pos(
+        self, episode, start_hold_obj_idx=None, force_idx=None
+    ):
+        # learn nav to pick skill if not holding object currently
+        if start_hold_obj_idx is None:
+            # starting positions of candidate objects
+            all_pos = np.stack(
+                [goal.position for goal in episode.candidate_objects],
+                axis=0,
+            )
+            if force_idx is not None:
+                raise NotImplementedError
+        else:
+            # positions of candidate goal receptacles
+            all_pos = np.stack(
+                [goal.position for goal in episode.candidate_goal_receps],
+                axis=0,
+            )
+
+        return all_pos
