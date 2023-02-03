b5141d394a3172b8505bbe1dbf8426788cd76867
vincentpierre <vincentpierre@users.noreply.github.com>
2023-02-03 15:56:52 -0800

    black reformat

--- scripts/generate_profile_shell_scripts.py
+++ scripts/generate_profile_shell_scripts.py
@@ -20,7 +20,6 @@ This script's intended usage is:
 import os
 
 if __name__ == "__main__":
-
     # The Habitat-lab program to be profiled (the command you usually use to
     # invoke it).
     program_str = "python -u -m habitat_baselines.run --exp-config habitat-baselines/habitat_baselines/config/pointnav/ddppo_pointnav.yaml --run-type train"
