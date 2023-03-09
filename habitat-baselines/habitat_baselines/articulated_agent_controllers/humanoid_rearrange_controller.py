5d8fda84bd5907d4263ac9acee98d8bc6965d067
Xavi Puig <xavierpuig@fb.com>
2023-03-09 00:20:12 -0800

    tests to stabilize controller

--- habitat-baselines/habitat_baselines/articulated_agent_controllers/humanoid_rearrange_controller.py
+++ habitat-baselines/habitat_baselines/articulated_agent_controllers/humanoid_rearrange_controller.py
@@ -76,6 +76,14 @@ class HumanoidRearrangeController:
         self.turning_step_amount = TURNING_STEP_AMOUNT
         self.threshold_rotate_not_move = THRESHOLD_ROTATE_NOT_MOVE
         self.base_offset = mn.Vector3(base_offset)
+        self.rotate_count = 0
+
+        self.curr_ind_map = {"cont1": 0, "cont2": 0, "cont": 0}
+        self._sim = None
+        # Initialize offset
+        self.transform_pose_offset = mn.Matrix4.from_(
+            mn.Matrix3.identity_init(), mn.Vector3()
+        )
 
         if not os.path.isfile(walk_pose_path):
             raise RuntimeError(
@@ -110,38 +118,106 @@ class HumanoidRearrangeController:
     def reset(self, position) -> None:
         """Reset the joints on the human. (Put in rest state)"""
         self.obj_transform.translation = position + self.base_offset
+        self.curr_ind_map = {
+            "cont1": self.curr_ind_map["cont1"] + 1,
+            "cont2": self.curr_ind_map["cont2"] + 1,
+            "cont": self.curr_ind_map["cont"] + 1,
+        }
 
-    def get_stop_pose(self):
+    def get_corrected_base(self, obj_transform: mn.Matrix4):
+        """
+        When performing a walking motion the character rotation and translation get shifted.
+        This function returns a base pose that accounts for that, to make sure that the nav path
+        does not get stuck.
+        """
+        offset = self.transform_pose_offset
+
+        # base_correction = mn.Matrix4.from_(
+        #     obj_transform.rotation() @ self.transform_pose_offset.rotation().transposed(),
+        #     obj_transform.translation-offset)
+
+        base_correction = mn.Matrix4.from_(
+            offset.rotation().transposed(),
+            -offset.rotation().transposed() * offset.translation,
+        )
+
+        base_T = obj_transform @ base_correction
+        base_pos = base_T.translation - self.base_offset
+        return base_pos, base_T
+
+    def get_stop_pose(self, obj_transform: mn.Matrix4 = None):
         """
         Returns a stop, standing pose
         """
+        if obj_transform is None:
+            obj_transform = self.obj_transform
+        self.obj_transform = obj_transform
         joint_pose = self.stop_pose.joints
         obj_transform = (
             self.obj_transform
         )  # the object transform does not change
         return joint_pose, obj_transform
 
-    def compute_turn(self, target_position: mn.Vector3):
+    def gen_map(self, p1, p2, c=1):
+        points = [p1, p2]
+        if c == 1:
+            colors = [mn.Color3.red()]
+        else:
+            colors = [mn.Color3.blue()]
+
+        if f"trajectory{c}" in self.curr_ind_map:
+            # pass
+            # print(self.curr_ind_map)
+            self._sim.get_rigid_object_manager().remove_object_by_id(
+                self.curr_ind_map[f"trajectory{c}"]
+            )
+        # breakpoint()
+        self.curr_ind_map[f"trajectory{c}"] = self._sim.add_trajectory_object(
+            "current{}_path_{}".format(c, self.curr_ind_map[f"cont{c}"]),
+            points,
+            color=colors[0],
+            radius=0.03,
+        )
+        self.curr_ind_map[f"cont{c}"] += 1
+
+    def compute_turn(
+        self, target_position: mn.Vector3, obj_transform: mn.Matrix4 = None
+    ):
         """
         Generate some motion without base transform, just turn
         """
-        return self.get_walk_pose(target_position, distance_multiplier=0)
+        return self.get_walk_pose(
+            target_position, obj_transform, distance_multiplier=0
+        )
 
     def get_walk_pose(
-        self, target_position: mn.Vector3, distance_multiplier=1.0
+        self,
+        target_position: mn.Vector3,
+        obj_transform: mn.Matrix4 = None,
+        distance_multiplier=1.0,
     ):
         """
         Computes a walking pose and transform, so that the humanoid moves to the relative position
 
         :param position: target position, relative to the character root translation
+        :param obj_transform: the current object transform
         :param distance_multiplier: allows to create walk motion while not translating, good for turning
         """
+        if obj_transform is None:
+            obj_transform = self.obj_transform
+        if self.rotate_count > 30:
+            print("TARGET", target_position)
+        self.obj_transform = obj_transform
         forward_V = target_position
         if forward_V.length() == 0.0:
-            return self.get_stop_pose()
+            breakpoint()
+            return self.get_stop_pose(obj_transform)
         distance_to_walk = np.linalg.norm(forward_V)
         did_rotate = False
 
+        curr_pos = obj_transform.translation - self.base_offset
+        # self.gen_map(curr_pos, curr_pos + target_position)
+        # self.gen_map(curr_pos, curr_pos + target_position, c=2)
         # current_pose = self.walk_motion.poses[self.walk_mocap_frame]
         # current_joint_pose, current_obj_transform = (
         #     current_pose.joints,
@@ -230,22 +306,58 @@ class HumanoidRearrangeController:
         # We correct the object transform
         obj_transform.translation *= 0
 
+        # Remove the forward component, and orient according to forward_V
+        obj_transform.translation *= mn.Vector3.x_axis() + mn.Vector3.y_axis()
+
+        self.transform_pose_offset = obj_transform
+
         look_at_path_T = mn.Matrix4.look_at(
             self.obj_transform.translation,
-            self.obj_transform.translation - forward_V.normalized(),
+            self.obj_transform.translation + forward_V.normalized(),
             mn.Vector3.y_axis(),
         )
 
-        # Remove the forward component, and orient according to forward_V
-        obj_transform.translation *= mn.Vector3.x_axis() + mn.Vector3.y_axis()
-        obj_transform = look_at_path_T  # @ obj_transform
+        look_at_path_T0 = mn.Matrix4.look_at(
+            mn.Vector3(),
+            mn.Vector3(0, 0, 1.0),
+            mn.Vector3.y_axis(),
+        )
+        self.transform_pose_offset = obj_transform
+        obj_transform = look_at_path_T @ obj_transform
+
+        # obj_transform_norot = look_at_path_T
+
+        # breakpoint()
         forward_V_dist = forward_V * dist_diff * distance_multiplier
         obj_transform.translation += forward_V_dist
         v1 = self.obj_transform.translation + forward_V_dist
         v2 = obj_transform.translation
-        if v1 != v2:
-            print("ERROR ALIGNMENT")
+        v2_corrected = self.get_corrected_base(obj_transform)[1]
+        # print(obj_transform_norot.rotation(), '-->', v2_corrected.rotation())
+        # print(obj_transform_norot.translation, '|-->', v2_corrected.translation)
+        # breakpoint()
+        # goal = obj_transform_norot
+        # offs = self.transform_pose_offset
+        # current = obj_transform
+        # breakpoint()
+        # if v1 != v2:
+        #     # pass
+        #     print("ERROR ALIGNMENT:", "should", v1, "did", v2)
+        #     v2_corrected = self.get_corrected_base(obj_transform)
+        #     print(v2_corrected[1].translation)
+        # breakpoint()
+        # if distance_multiplier == 0:
+        #     breakpoint()
         self.obj_transform = obj_transform
+
+        if self.rotate_count > 30:
+            pass
+            # breakpoint()
+
+        if did_rotate:
+            self.rotate_count += 1
+        else:
+            self.rotate_count = 0
 
         return joint_pose, obj_transform
 
--- habitat-lab/habitat/tasks/rearrange/actions/oracle_nav_action.py
+++ habitat-lab/habitat/tasks/rearrange/actions/oracle_nav_action.py
@@ -44,6 +44,8 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
         self._prev_ep_id = None
         self._targets = {}
 
+        self.curr_ind_map = {"cont": 0}
+
     @staticmethod
     def _compute_turn(rel, turn_vel, robot_forward):
         is_left = np.cross(robot_forward, rel) > 0
@@ -73,6 +75,8 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
             self._targets = {}
             self._prev_ep_id = self._task._episode_id
 
+        self.curr_ind_map = {"cont": self.curr_ind_map["cont"] + 1}
+
     def _get_target_for_idx(self, nav_to_target_idx: int):
         if nav_to_target_idx not in self._targets:
             nav_to_obj = self._poss_entities[nav_to_target_idx]
@@ -94,8 +98,9 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
             self._targets[nav_to_target_idx] = (start_pos, np.array(obj_pos))
         return self._targets[nav_to_target_idx]
 
-    def _path_to_point(self, point):
-        agent_pos = self.cur_articulated_agent.base_pos
+    def _path_to_point(self, point, agent_pos=None):
+        if agent_pos is None:
+            agent_pos = self.cur_articulated_agent.base_pos
 
         path = habitat_sim.ShortestPath()
         path.requested_start = agent_pos
@@ -106,6 +111,7 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
         return path.points
 
     def step(self, *args, is_last_action, **kwargs):
+        self.humanoid_controller._sim = self._sim
         nav_to_target_idx = kwargs[
             self._action_arg_prefix + "oracle_nav_action"
         ]
@@ -121,7 +127,33 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
         final_nav_targ, obj_targ_pos = self._get_target_for_idx(
             nav_to_target_idx
         )
-        curr_path_points = self._path_to_point(final_nav_targ)
+        if self.motion_type != "base_velocity":
+            base_T = self.cur_articulated_agent.base_transformation
+            base_pos, _ = self.humanoid_controller.get_corrected_base(base_T)
+            bpos = self.cur_articulated_agent.base_transformation.translation
+            # print("CORRECTED", base_pos, "BASEPOS", bpos)
+            # breakpoint()
+            curr_path_points = self._path_to_point(final_nav_targ, base_pos)
+        else:
+            curr_path_points = self._path_to_point(final_nav_targ)
+
+        colors = [mn.Color3.red(), mn.Color3.yellow(), mn.Color3.green()]
+
+        if "trajectory" in self.curr_ind_map:
+            # pass
+            # print(self.curr_ind_map)
+            self._sim.get_rigid_object_manager().remove_object_by_id(
+                self.curr_ind_map["trajectory"]
+            )
+        self.curr_ind_map[
+            "trajectory"
+        ] = self._sim.add_gradient_trajectory_object(
+            "current_path_{}".format(self.curr_ind_map["cont"]),
+            curr_path_points,
+            colors=colors,
+            radius=0.03,
+        )
+        self.curr_ind_map["cont"] += 1
 
         if curr_path_points is None:
             raise Exception
@@ -130,6 +162,25 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
 
             robot_pos = np.array(self.cur_articulated_agent.base_pos)
             base_T = self.cur_articulated_agent.base_transformation
+
+            if self.motion_type != "base_velocity":
+                # Need to correct base and robot_pos
+                # print(base_T)
+                (
+                    robot_pos2,
+                    base_T2,
+                ) = self.humanoid_controller.get_corrected_base(base_T)
+                # breakpoint()
+                forward = np.array([0.0, 0, 1.0])
+                fw1 = np.array(base_T.transform_vector(forward))
+                fw2 = np.array(base_T2.transform_vector(-forward))
+                self.humanoid_controller.gen_map(robot_pos, robot_pos + fw1)
+                self.humanoid_controller.gen_map(
+                    robot_pos2, robot_pos2 + fw2, c=2
+                )
+                print(robot_pos, robot_pos2)
+                robot_pos, base_T = robot_pos2, base_T2
+
             forward = np.array([1.0, 0, 0])
             robot_forward = np.array(base_T.transform_vector(forward))
 
@@ -139,6 +190,9 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
             robot_forward = robot_forward[[0, 2]]
             rel_targ = rel_targ[[0, 2]]
             rel_pos = (obj_targ_pos - robot_pos)[[0, 2]]
+
+            vec1 = mn.Vector3([rel_targ[0], 0.0, rel_targ[1]])
+            # self.humanoid_controller.gen_map(robot_pos, robot_pos + vec1, c=2)
 
             angle_to_target = get_angle(robot_forward, rel_targ)
             angle_to_obj = get_angle(robot_forward, rel_pos)
@@ -150,7 +204,9 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
                 dist_to_final_nav_targ < self._config.dist_thresh
                 and angle_to_obj < self._config.turn_thresh
             )
-
+            if self.humanoid_controller.rotate_count > 30:
+                print("NAV TARGET", dist_to_final_nav_targ, angle_to_obj)
+                print("", self._config.dist_thresh, self._config.turn_thresh)
             if self.motion_type == "base_velocity":
                 if not at_goal:
                     if dist_to_final_nav_targ < self._config.dist_thresh:
@@ -176,6 +232,7 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
             else:
                 if not at_goal:
                     if dist_to_final_nav_targ < self._config.dist_thresh:
+                        print("TURN to obj!!")
                         # Look at the object
                         (
                             new_pos,
@@ -196,6 +253,8 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
                         new_pos,
                         new_trans,
                     ) = self.humanoid_controller.get_stop_pose()
+                if new_trans is None:
+                    breakpoint()
                 base_action = self.humanoid_controller.vectorize_pose(
                     new_pos, new_trans
                 )
