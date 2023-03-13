4fb068b8c2540dab5b2ba20a74da8af5981578e8
vincentpierre <vincentpierre@users.noreply.github.com>
2023-03-13 11:30:24 -0700

    Moving to python 3.9

--- habitat-baselines/setup.py
+++ habitat-baselines/setup.py
@@ -61,8 +61,6 @@ if __name__ == "__main__":
             "Topic :: Scientific/Engineering :: Artificial Intelligence",
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Intended Audience :: Developers",
