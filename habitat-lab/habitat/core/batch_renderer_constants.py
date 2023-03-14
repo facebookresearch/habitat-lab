d89c87a24ce52ce45882cf7c39f673b65820a4b5
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2023-03-14 16:14:10 -0400

    Formatting fix.

--- habitat-lab/habitat/core/batch_renderer_constants.py
+++ habitat-lab/habitat/core/batch_renderer_constants.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import TYPE_CHECKING
+
 from typing_extensions import Final
 
 RENDER_STATE_OBSERVATION_KEY: Final[str] = "render_state"
