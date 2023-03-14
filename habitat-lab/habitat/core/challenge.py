92901ece6cb93e5d3a38822457843595409c6e76
Karmesh Yadav <ykarmesh@gmail.com>
2023-03-14 13:11:13 -0700

    added my solution to the action configs issue (#1180)
    * added my solution to the action configs issue
    
    * added the grpc change
    
    ---------
    
    Co-authored-by: Karmesh Yadav <karmeshyadav@fb.com>

--- habitat-lab/habitat/core/challenge.py
+++ habitat-lab/habitat/core/challenge.py
@@ -11,9 +11,9 @@ from habitat.core.logging import logger
 
 
 class Challenge(Benchmark):
-    def __init__(self, eval_remote=False):
+    def __init__(self, eval_remote=False, action_space=None):
         config_paths = os.environ["CHALLENGE_CONFIG_FILE"]
-        super().__init__(config_paths, eval_remote=eval_remote)
+        super().__init__(config_paths, eval_remote=eval_remote, action_space=action_space)
 
     def submit(self, agent):
         metrics = super().evaluate(agent)
