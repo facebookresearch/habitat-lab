d61549d808f6e350de6450e4e767ff64d3180cb5
vincentpierre <vincentpierre@users.noreply.github.com>
2023-03-15 14:48:07 -0700

    readding MIT

--- habitat_baselines/rl/ddppo/policy/resnet_bpsnav.py
+++ habitat_baselines/rl/ddppo/policy/resnet_bpsnav.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 # Copyright (c) Meta Platforms, Inc. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from functools import partial
 
