d41c1d9376bf274b893aec4f2b7c55aee67f77df
Andrew Szot <ASzot@users.noreply.github.com>
2023-03-16 15:41:53 -0700

    Multi-Agent RL Trainers [Part 2 of Multi-Agent RL] (#1170)
    * Added TrainerAgent
    
    * VER changes
    
    * Consolidated into Agent
    
    * Integrated VER trainer
    
    * Fixed CI
    
    * Added pop play trainer
    
    * Refactored agent access interface
    
    * Functioning
    
    * Pop play running fine
    
    * Pre-commit fixes
    
    * Updated naming
    
    * removed multi-agent
    
    * Removed unnecessary MA file
    
    * Added doc
    
    * merged
    
    * Revert "Removed unnecessary MA file"
    
    This reverts commit b5b164c9e03ca97e0c8c7a0650b1be0e9c9505e1.
    
    * Fixed VER import
    
    * Address PR comments
    
    * precommit

--- habitat-baselines/habitat_baselines/config/default_structured_configs.py
+++ habitat-baselines/habitat_baselines/config/default_structured_configs.py
@@ -358,6 +358,14 @@ class DDPPOConfig(HabitatBaselinesBaseConfig):
 @dataclass
 class AgentAccessMgrConfig(HabitatBaselinesBaseConfig):
     type: str = "SingleAgentAccessMgr"
+    ###############################
+    # Population play configuration
+    num_total_agents: int = 1
+    num_active_agents: int = 1
+    agent_sample_interval: int = 20
+    allow_self_play: bool = False
+    self_play_batched: bool = False
+    ###############################
 
 
 @dataclass
