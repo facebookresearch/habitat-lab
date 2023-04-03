3c730c5b996e4db2ed2d873b47a08053fb35320d
Xavier Puig <xavierpuigf@gmail.com>
2023-04-03 10:28:42 -0700

    Update habitat-baselines/habitat_baselines/rl/ddppo/policy/resnet_policy.py
    Co-authored-by: Vincent-Pierre BERGES <28320361+vincentpierre@users.noreply.github.com>

--- habitat-baselines/habitat_baselines/rl/ddppo/policy/resnet_policy.py
+++ habitat-baselines/habitat_baselines/rl/ddppo/policy/resnet_policy.py
@@ -118,7 +118,7 @@ class PointNavResNetPolicy(NetPolicy):
         if agent_name is None:
             if len(config.habitat.simulator.agents_order) > 1:
                 raise ValueError(
-                    "If there is more than an agent, you should specify the agent name"
+                    "If there is more than an agent, you need to specify the agent name"
                 )
             else:
                 agent_name = config.habitat.simulator.agents_order[0]
