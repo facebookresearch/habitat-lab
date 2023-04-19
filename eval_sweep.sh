769a2a1927d639066e5af605cba6622f49ea2cdb
akshararai <akshara.rai@gmail.com>
2023-04-19 13:26:11 -0700

    SIRo eval sweep (#1276)
    * add a bash file to sweep for eval
    
    * add a bash file to sweep for eval
    
    * set load_ckpt True by default during eval
    
    * added should_load_ckpt to be True
    
    ---------
    
    Co-authored-by: Akshara Rai <akshararai@devfair0196.h2.fair>

--- eval_sweep.sh
+++ eval_sweep.sh
@@ -11,5 +11,5 @@ for dir in "$SWEEP_DIR"/*/; do
 	subdir=$(basename "$dir")
     echo "$subdir"
     wandb_name="eval_$SWEEP_SUBDIR"
-    python habitat-baselines/habitat_baselines/run.py -m --config-name config.yaml --config-path "${dir}"/.hydra/ habitat_baselines.eval_ckpt_path_dir="${dir}"/checkpoints/latest.pth habitat_baselines.video_dir="${dir}"/video habitat_baselines.wb.run_name="${wandb_name}" habitat_baselines.evaluate=True habitat_baselines.test_episode_count=20 habitat_baselines.num_environments=16
+    python habitat-baselines/habitat_baselines/run.py -m --config-name config.yaml --config-path "${dir}"/.hydra/ habitat_baselines.eval_ckpt_path_dir="${dir}"/checkpoints/latest.pth habitat_baselines.video_dir="${dir}"/video habitat_baselines.wb.run_name="${wandb_name}" habitat_baselines.evaluate=True habitat_baselines.test_episode_count=20 habitat_baselines.num_environments=16 habitat_baselines.eval.should_load_ckpt=True
 done
