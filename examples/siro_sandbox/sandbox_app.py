f52145b4fda9853f3cc48c321949da64c9dae6b3
Eric Undersander <eundersander@fb.com>
2023-05-09 14:15:00 -0700

    fix for how we restrict humanoid movement to navmesh; fix for too-short path returned from pathfinder (#1324)

--- examples/siro_sandbox/sandbox_app.py
+++ examples/siro_sandbox/sandbox_app.py
@@ -311,7 +311,7 @@ class SandboxDriver(GuiAppDriver):
         path.requested_end = snapped_pos
         found_path = pathfinder.find_path(path)
 
-        if not found_path:
+        if not found_path or len(path.points) < 2:
             return None
 
         path_points = []
