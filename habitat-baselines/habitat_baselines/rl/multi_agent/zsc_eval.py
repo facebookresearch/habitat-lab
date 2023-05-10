e510217be0d9b4557754629fa230fceddcdc7ed1
Andrew Szot <ASzot@users.noreply.github.com>
2023-05-10 09:38:56 -0700

    SIRo Evaluation Fixes (#1314)
    * Fixing habitat configs
    
    * update
    
    * ZSC evalutation script
    
    * ZSC evalutation script
    
    * Added easier bdp eval
    
    * update how skill envs
    
    * update how nav skill ends
    
    * mini fix
    
    * Tracking current step
    
    * fix for single agent training (#1320)
    
    Co-authored-by: Akshara Rai <akshararai@devfair0196.h2.fair>
    
    * get proper agent count for pop play
    
    * Fix single agent. Added binary format for episodes
    
    * Spot nav (#1305)
    
    * add oracle action
    
    * pddl fix
    
    * fix
    
    * spot nav action
    
    * remove yaml
    
    * fix precommit issue
    
    * fix PR review feedback
    
    * fix trans issue
    
    * change the name of the action
    
    * update readme and error
    
    * solve the teleporting issue and early termination
    
    * change max step
    
    * add contact test
    
    * incrase skill max step
    
    * allow the agent to consider current location when snap
    
    * move the navmesh code in a seperate util function
    
    * fix action name in oracle nav
    
    * remove pddl modification, turn off post condition
    
    * remove blank
    
    * support multi-agent
    
    * add yaml file for the multi-agent
    
    * udpate readme for multi-agent running
    
    * oracle nav
    
    * fix precommit
    
    * add minitrain and string
    
    * add yaml
    
    * Update habitat-baselines/habitat_baselines/rl/hrl/hl/planner_policy.py
    
    Co-authored-by: Aaron Gokaslan <aaronGokaslan@gmail.com>
    
    * More docstring
    
    * CI
    
    ---------
    
    Co-authored-by: xavierpuigf <xavierpuigf@gmail.com>
    Co-authored-by: Xavier Puig <xavierpuig@fb.com>
    Co-authored-by: akshararai <akshara.rai@gmail.com>
    Co-authored-by: Akshara Rai <akshararai@devfair0196.h2.fair>
    Co-authored-by: Jimmy Yang <55121504+jimmytyyang@users.noreply.github.com>
    Co-authored-by: Aaron Gokaslan <aaronGokaslan@gmail.com>

---
+++ habitat-baselines/habitat_baselines/rl/multi_agent/zsc_eval.py
@@ -0,0 +1,20 @@
+import argparse
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # For rearrange-easy there are 5 plans.
+    parser.add_argument("--num-plans", type=int, default=5)
+    parser.add_argument(
+        "opts",
+        default=None,
+        nargs=argparse.REMAINDER,
+        help="Modify config options from command line",
+    )
+    args = parser.parse_args()
+    add_opts = " ".join(args.opts)
+
+    for plan_idx in range(args.num_plans):
+        run_cmd = f"python habitat_baselines/run.py --config-name=experiments_hab3/eval_zsc_kinematic_oracle.yaml habitat_baselines.rl.policy.agent_1.hierarchical_policy.high_level_policy.plan_idx={plan_idx} {add_opts}"
+        print(f"RUNNING {run_cmd}")
+        os.system(run_cmd)
