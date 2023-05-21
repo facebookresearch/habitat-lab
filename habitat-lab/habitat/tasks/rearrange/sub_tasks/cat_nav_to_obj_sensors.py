61c34a340c4e08bda53af52531d1b0600d968c40
Arun Ramachandran <arun.jhurricane@gmail.com>
2023-05-20 22:18:01 -0400

    [OVMM] nav fixes (#1353)
    * OVMM nav fixes
    
    - Use categories from separate file and episodes for receptacle
      semantic IDs
    - Set default camera tilt to -30 degrees
    
    * Don't load whole dataset when split across scenes
    
    * Replace hard-coded joint angles
    
    * Cleaner check for loaded recep cats
    
    * Fix OVMM goal seg sensor

--- habitat-lab/habitat/tasks/rearrange/sub_tasks/cat_nav_to_obj_sensors.py
+++ habitat-lab/habitat/tasks/rearrange/sub_tasks/cat_nav_to_obj_sensors.py
@@ -81,7 +81,7 @@ class OvmmNavGoalSegmentationSensor(Sensor):
                 obj_id = rom.get_object_id_by_handle(handle)
             instance_id = obj_id + self._instance_ids_start
             # Skip if object is not in the agent's viewport
-            if instance_id >= max_obs_val:
+            if instance_id > max_obs_val:
                 continue
             obs[pan_obs == instance_id] = 1
         return obs
@@ -165,6 +165,9 @@ class ReceptacleSegmentationSensor(Sensor):
     ):
         obs = np.copy(observations[self.panoptic_uuid])
         obj_id_map = np.zeros(np.max(obs) + 1, dtype=np.int32)
+        assert (
+            task.loaded_receptacle_categories
+        ), "Empty receptacle semantic IDs, task didn't cache them."
         for obj_id, semantic_id in task.receptacle_semantic_ids.items():
             instance_id = obj_id + self._instance_ids_start
             # Skip if receptacle is not in the agent's viewport
