7ff4133cd71ddf356d1fedf20e55dbf5d496cb59
Xavi Puig <xavierpuigf@gmail.com>
2023-05-22 10:19:57 -0700

    access only config on init

--- habitat-lab/habitat/tasks/rearrange/rearrange_task.py
+++ habitat-lab/habitat/tasks/rearrange/rearrange_task.py
@@ -79,6 +79,12 @@ class RearrangeTask(NavigationTask):
         self._episode_id: str = ""
         self._cur_episode_step = 0
         self._should_place_articulated_agent = should_place_articulated_agent
+        self._fixed_starting_position: bool = False
+        self._seed = self._sim.habitat_config.seed
+
+        habitat_config = self._sim.habitat_config
+        if "overfit" in habitat_config and habitat_config["overfit"]:
+            self._fixed_starting_position = True
 
         data_path = dataset.config.data_path.format(split=dataset.config.split)
         fname = data_path.split("/")[-1].split(".")[0]
@@ -187,12 +193,9 @@ class RearrangeTask(NavigationTask):
             self._is_episode_active = True
 
             if self._should_place_articulated_agent:
-                
-                habitat_config = self._sim.habitat_config
-                if "overfit" in habitat_config and habitat_config["overfit"]: 
-                    curr_seed = self._sim.habitat_config.seed
-                    np.random.seed(curr_seed)
-                    self._sim.pathfinder.seed(curr_seed)
+                if self._fixed_starting_position: 
+                    np.random.seed(self._seed)
+                    self._sim.pathfinder.seed(self._seed)
                     
                 for agent_idx in range(self._sim.num_articulated_agents):
                     self._set_articulated_agent_start(agent_idx)
