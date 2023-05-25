b3ef5c3c1ecdd70ab5ee3585a4246ba859cabd5b
Sriram Yenamandra <yvsriram99@gmail.com>
2023-05-25 18:30:58 -0400

    update configs, hardcode picked idx (TODO)

--- habitat-lab/habitat/tasks/rearrange/rearrange_task.py
+++ habitat-lab/habitat/tasks/rearrange/rearrange_task.py
@@ -74,7 +74,7 @@ class RearrangeTask(NavigationTask):
         self._episode_id: str = ""
         self._cur_episode_step = 0
         self._should_place_robot = should_place_robot
-        self._picked_object_idx = None
+        self._picked_object_idx = 0             # TODO:
         self._in_manip_mode = False
         data_path = dataset.config.data_path.format(split=dataset.config.split)
         fname = data_path.split("/")[-1].split(".")[0]
