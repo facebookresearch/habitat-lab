e7c17e494eecae3766de175b07e063817ec8ab0b
Mikaël Dallaire Côté <110583667+0mdc@users.noreply.github.com>
2023-06-02 09:09:03 -0400

    SIRo: Port NavMeshSettings refactor (#1394)
    * Port breaking navmesh changes to SIRo.
    
    * another update
    
    ---------
    
    Co-authored-by: aclegg3 <alexanderwclegg@gmail.com>

--- test/test_robot_wrapper.py
+++ test/test_robot_wrapper.py
@@ -305,7 +305,8 @@ def test_fetch_robot_wrapper(fixed_base):
         # compute a navmesh on the ground plane
         navmesh_settings = habitat_sim.NavMeshSettings()
         navmesh_settings.set_defaults()
-        sim.recompute_navmesh(sim.pathfinder, navmesh_settings, True)
+        navmesh_settings.include_static_objects = True
+        sim.recompute_navmesh(sim.pathfinder, navmesh_settings)
         sim.navmesh_visualization = True
 
         # add the robot to the world via the wrapper
@@ -459,7 +460,8 @@ def test_franka_robot_wrapper():
         # compute a navmesh on the ground plane
         navmesh_settings = habitat_sim.NavMeshSettings()
         navmesh_settings.set_defaults()
-        sim.recompute_navmesh(sim.pathfinder, navmesh_settings, True)
+        navmesh_settings.include_static_objects = True
+        sim.recompute_navmesh(sim.pathfinder, navmesh_settings)
         sim.navmesh_visualization = True
 
         # add the robot to the world via the wrapper
@@ -580,7 +582,8 @@ def test_spot_robot_wrapper(fixed_base):
         # compute a navmesh on the ground plane
         navmesh_settings = habitat_sim.NavMeshSettings()
         navmesh_settings.set_defaults()
-        sim.recompute_navmesh(sim.pathfinder, navmesh_settings, True)
+        navmesh_settings.include_static_objects = True
+        sim.recompute_navmesh(sim.pathfinder, navmesh_settings)
         sim.navmesh_visualization = True
         # add the robot to the world via the wrapper
         robot_path = "data/robots/hab_spot_arm/urdf/hab_spot_arm.urdf"
@@ -718,7 +721,8 @@ def test_stretch_robot_wrapper(fixed_base):
         # compute a navmesh on the ground plane
         navmesh_settings = habitat_sim.NavMeshSettings()
         navmesh_settings.set_defaults()
-        sim.recompute_navmesh(sim.pathfinder, navmesh_settings, True)
+        navmesh_settings.include_static_objects = True
+        sim.recompute_navmesh(sim.pathfinder, navmesh_settings)
         sim.navmesh_visualization = True
         # add the robot to the world via the wrapper
         robot_path = "data/robots/hab_stretch/urdf/hab_stretch.urdf"
