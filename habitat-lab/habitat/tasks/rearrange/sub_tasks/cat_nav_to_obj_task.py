b8309aface78b8651d1bc4bf37d62fd325be5878
Sriram Yenamandra <yvsriram99@gmail.com>
2023-06-13 08:34:05 -0400

    fix receptacle segmentation sensor

--- habitat-lab/habitat/tasks/rearrange/sub_tasks/cat_nav_to_obj_task.py
+++ habitat-lab/habitat/tasks/rearrange/sub_tasks/cat_nav_to_obj_task.py
@@ -48,8 +48,9 @@ class CatDynNavRLEnv(DynNavRLEnv):
         return self._loaded_receptacle_categories
 
     def reset(self, episode: Episode):
-        obs = super().reset(episode)
+        self._receptacle_semantic_ids = {}
         self._cache_receptacles()
+        obs = super().reset(episode)
         self._nav_to_obj_goal = np.stack(
             [
                 view_point.agent_state.position
