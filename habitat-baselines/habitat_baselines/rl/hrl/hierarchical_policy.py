bcc90d630e2d4f5dbbc261e1819bf8a38fe9d287
Theo Gervet <theop123@meta.com>
2023-06-26 16:48:12 -0700

    Add socnav

--- habitat-baselines/habitat_baselines/rl/hrl/hierarchical_policy.py
+++ habitat-baselines/habitat_baselines/rl/hrl/hierarchical_policy.py
@@ -415,11 +415,14 @@ class HierarchicalPolicy(nn.Module, Policy):
                     rnn_hidden_states[batch_ids] = hl_info[
                         "rnn_hidden_states"
                     ][batch_ids]
-                    print("type(prev_actions[batch_ids])", prev_actions[batch_ids].dtype)
-                    print("type(hl_info[actions][batch_ids])", hl_info["actions"][batch_ids].dtype)
                     if prev_actions[batch_ids].dtype != hl_info["actions"][batch_ids].dtype:
-                        print("self._high_level_policy._agent_name", self._high_level_policy._agent_name)
-                    prev_actions[batch_ids] = hl_info["actions"][batch_ids]
+                        print("type(prev_actions[batch_ids])",
+                              prev_actions[batch_ids].dtype)
+                        print("type(hl_info[actions][batch_ids])",
+                              hl_info["actions"][batch_ids].dtype)
+                        print("self._high_level_policy._agent_name",
+                              self._high_level_policy._agent_name)
+                    prev_actions[batch_ids] = hl_info["actions"][batch_ids].to(prev_actions[batch_ids].dtype)
                 elif self._skills[skill_id].has_hidden_state:
                     raise ValueError(
                         f"The code does not currently support neural LL and neural HL skills. Skill={self._skills[skill_id]}, HL={self._high_level_policy}"
