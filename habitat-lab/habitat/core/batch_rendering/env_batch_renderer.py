68b72821d93eb03b2fdaf087ac7678756bef5201
ASzot <ASzot@users.noreply.github.com>
2023-07-10 09:21:32 -0700

    Added contig fix

--- habitat-lab/habitat/core/batch_rendering/env_batch_renderer.py
+++ habitat-lab/habitat/core/batch_rendering/env_batch_renderer.py
@@ -169,7 +169,7 @@ class EnvBatchRenderer:
                 env_observations
             )
             for key, value in processed_obs.items():
-                env_observations[key] = value
+                env_observations[key] = np.ascontiguousarray(value)
             output.append(env_observations)
         return output
 
