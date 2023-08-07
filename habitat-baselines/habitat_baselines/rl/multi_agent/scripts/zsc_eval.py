55c6ccee2a2832941655b2ec258a19a87047d971
ASzot <ASzot@users.noreply.github.com>
2023-08-06 22:24:25 -0700

    Learned agents in zsc

--- habitat-baselines/habitat_baselines/rl/multi_agent/scripts/zsc_eval.py
+++ habitat-baselines/habitat_baselines/rl/multi_agent/scripts/zsc_eval.py
@@ -4,10 +4,34 @@ import random
 import string
 import uuid
 
+
+def zsc_eval(args, add_opts):
+    rnd_id = random.choice(string.ascii_uppercase) + str(uuid.uuid4())[:8]
+
+    for plan_idx in range(args.num_plans):
+        run_cmd = f"python habitat_baselines/run.py --config-name=experiments_hab3/eval_zsc_kinematic_oracle.yaml habitat_baselines.rl.policy.agent_1.hierarchical_policy.high_level_policy.plan_idx={plan_idx} {add_opts} habitat_baselines.wb.group={rnd_id}"
+        print(f"RUNNING {run_cmd}")
+        os.system(run_cmd)
+        if args.debug:
+            break
+
+    learned_agents = args.learned_agents.split(",")
+    for learned_agent in learned_agents:
+        run_cmd = f"python habitat_baselines/run.py --config-name=experiments_hab3/pop_play_kinematic_oracle_humanoid_spot_fp.yaml habitat_baselines.agent.load_type1_pop_ckpts=[{learned_agent}] {add_opts} habitat_baselines.wb.group={rnd_id}"
+        print(f"RUNNING {run_cmd}")
+        os.system(run_cmd)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # For tidy-house style task there are 5 plans.
     parser.add_argument("--num-plans", type=int, default=5)
+    parser.add_argument(
+        "--learned-agents",
+        type=str,
+        default="",
+        help="Comma separated list of checkpoints of holdout learned agents.",
+    )
     parser.add_argument("--debug", action="store_true")
     parser.add_argument(
         "opts",
@@ -18,11 +42,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     add_opts = " ".join(args.opts)
 
-    rnd_id = random.choice(string.ascii_uppercase) + str(uuid.uuid4())[:8]
-
-    for plan_idx in range(args.num_plans):
-        run_cmd = f"python habitat_baselines/run.py --config-name=experiments_hab3/eval_zsc_kinematic_oracle.yaml habitat_baselines.rl.policy.agent_1.hierarchical_policy.high_level_policy.plan_idx={plan_idx} {add_opts} habitat_baselines.wb.group={rnd_id}"
-        print(f"RUNNING {run_cmd}")
-        os.system(run_cmd)
-        if args.debug:
-            break
+    zsc_eval(args, add_opts)
