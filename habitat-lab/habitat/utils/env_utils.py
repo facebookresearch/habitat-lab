477ed181776dc831005373b19a0f0f71b7a5ecd1
Ruslan <partsey2412@gmail.com>
2023-08-11 09:48:23 +0300

    SIRo HITL tool policy eval fix and refactor (#1479)
    * init SingleAgentAccessMgr
    * baselines controller refactor
    * set not_done_masks to True after the first action
    * removed temp --sample-random-baseline-base-vel argument
    * fixed ControllerHelper.controllers instantiation
    * removed unused code; added type annotations
    * ignore type error
    * reorganized controllers into separate files
    * addressing comments
    * refactored controllers' actions to be np.array
    * post-rebase fix

--- habitat-lab/habitat/utils/env_utils.py
+++ habitat-lab/habitat/utils/env_utils.py
@@ -4,17 +4,19 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import TYPE_CHECKING, Type, Union
+from typing import TYPE_CHECKING, Optional, Type, Union
 
+from habitat.core.dataset import Dataset
 from habitat.core.env import Env, RLEnv
-from habitat.datasets import make_dataset
 
 if TYPE_CHECKING:
     from omegaconf import DictConfig
 
 
 def make_env_fn(
-    config: "DictConfig", env_class: Union[Type[Env], Type[RLEnv]]
+    config: "DictConfig",
+    env_class: Union[Type[Env], Type[RLEnv]],
+    dataset: Optional[Dataset] = None,
 ) -> Union[Env, RLEnv]:
     r"""Creates an env of type env_class with specified config and rank.
     This is to be passed in as an argument when creating VectorEnv.
@@ -29,7 +31,8 @@ def make_env_fn(
     """
     if "habitat" in config:
         config = config.habitat
-    dataset = make_dataset(config.dataset.type, config=config.dataset)
+
     env = env_class(config=config, dataset=dataset)
     env.seed(config.seed)
+
     return env
