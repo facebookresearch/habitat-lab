d3171448aa872312e5854d5c8b76de0c78fac059
Xavier Puig <xavierpuigf@gmail.com>
2023-08-17 11:24:17 -0700

    SIRo (#1507)
    * SIRo
    
    * lint

--- habitat-baselines/habitat_baselines/rl/hrl/skills/__init__.py
+++ habitat-baselines/habitat_baselines/rl/hrl/skills/__init__.py
@@ -12,10 +12,6 @@ from habitat_baselines.rl.hrl.skills.pick import PickSkillPolicy
 from habitat_baselines.rl.hrl.skills.place import PlaceSkillPolicy
 from habitat_baselines.rl.hrl.skills.reset import ResetArmSkill
 from habitat_baselines.rl.hrl.skills.skill import SkillPolicy
-from habitat_baselines.rl.hrl.skills.turn import (
-    MoveSkillPolicy,
-    TurnSkillPolicy,
-)
 from habitat_baselines.rl.hrl.skills.wait import WaitSkillPolicy
 
 __all__ = [
@@ -30,6 +26,5 @@ __all__ = [
     "SkillPolicy",
     "WaitSkillPolicy",
     "NoopSkillPolicy",
-    "TurnSkillPolicy",
     "MoveSkillPolicy",
 ]
