bc3cf6c79c54cc90a1fd8d4ce65f63edc231133d
xavierpuigf <xavierpuigf@gmail.com>
2023-08-22 00:25:32 +0000

    humanoid random walk

---
+++ habitat-baselines/habitat_baselines/rl/hrl/hl/rand_walk_policy.py
@@ -0,0 +1,81 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import List, Tuple
+
+import torch
+
+from habitat_baselines.rl.hrl.hl.high_level_policy import HighLevelPolicy
+
+
+class RandomWalkHighLevelPolicy(HighLevelPolicy):
+    """
+    Executes a fixed sequence of high-level actions as specified by the
+    `solution` field of the PDDL problem file.
+    :property _solution_actions: List of tuples were first tuple element is the
+        action name and the second is the action arguments.
+    """
+
+    _solution_actions: List[Tuple[str, List[str]]]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.skill_name = "nav_to_randcoord"
+
+    def filter_envs(self, curr_envs_to_keep_active):
+        """
+        Cleans up stateful variables of the policy so that
+        they match with the active environments
+        """
+
+    def get_value(self, observations, rnn_hidden_states, prev_actions, masks):
+        # We assign a value of 0. This is needed so that we can concatenate values in multiagent
+        # policies
+        return torch.zeros(rnn_hidden_states.shape[0], 1).to(
+            rnn_hidden_states.device
+        )
+
+    def get_next_skill(
+        self,
+        observations,
+        rnn_hidden_states,
+        prev_actions,
+        masks,
+        plan_masks,
+        deterministic,
+        log_info,
+    ):
+        batch_size = masks.shape[0]
+        next_skill = torch.zeros(batch_size)
+        skill_args_data = [None for _ in range(batch_size)]
+        immediate_end = torch.zeros(batch_size, dtype=torch.bool)
+        skill_name = self.skill_name
+        for batch_idx, should_plan in enumerate(plan_masks):
+            if should_plan == 1.0:
+                if skill_name not in self._skill_name_to_idx:
+                    raise ValueError(
+                        f"Could not find skill named {skill_name} in {self._skill_name_to_idx}"
+                    )
+                next_skill[batch_idx] = self._skill_name_to_idx[skill_name]
+                skill_args_data[batch_idx] = 1  # type: ignore[call-overload]
+
+        return next_skill, skill_args_data, immediate_end, {}
+
+    def on_enter(
+        self,
+        skill_arg,
+        batch_idx,
+        observations,
+        rnn_hidden_states,
+        prev_actions,
+    ):
+        self._is_target_obj = None
+        self._targ_obj_idx = None
+        self._prev_angle = {}
+
+        ret = super().on_enter(
+            skill_arg, batch_idx, observations, rnn_hidden_states, prev_actions
+        )
+        self._was_running_on_prev_step = False
+        return ret
