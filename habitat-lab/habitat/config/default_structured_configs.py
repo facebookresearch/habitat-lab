6e77250d727d27dc99e9e8f7aad92f57e890f4f1
ASzot <ASzot@users.noreply.github.com>
2023-09-06 17:15:45 -0700

    Merged

--- habitat-lab/habitat/config/default_structured_configs.py
+++ habitat-lab/habitat/config/default_structured_configs.py
@@ -1079,7 +1079,7 @@ class NumAgentsCollideConfig(MeasurementConfig):
 
 
 @dataclass
-class CooperateSubgoalRewardConfig(CompositeSubgoalReward):
+class CooperateSubgoalRewardConfig(PddlSubgoalReward):
     type: str = "CooperateSubgoalReward"
     stage_sparse_reward: float = 1.0
     end_on_collide: bool = True
