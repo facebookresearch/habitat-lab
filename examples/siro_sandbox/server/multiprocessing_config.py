185591a4b19f2b989e1a142fe5f7a0384da9ebf6
Eric Undersander <eundersander@meta.com>
2023-09-08 22:56:41 -0400

    add server files

---
+++ examples/siro_sandbox/server/multiprocessing_config.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+# multiprocessing.dummy uses multithreading instead of multiprocessing
+use_dummy = True
+
+# User code should import this file and use e.g. multiprocessing_config.Process. Thus
+# user code doesn't have to know whether we're using multiprocessing.dummy or
+# multiprocessing.
+
+Process: Any
+Queue: Any
+Semaphore: Any
+
+if use_dummy:
+    from multiprocessing.dummy import Process, Queue, Semaphore  # noqa: 0
+else:
+    from multiprocessing import Process, Queue, Semaphore  # noqa: 0
