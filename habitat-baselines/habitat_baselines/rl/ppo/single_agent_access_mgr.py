28a4e47ef4b84e8decc057582e87b8c6e78c205f
xavierpuigf <xavierpuigf@gmail.com>
2023-09-19 06:16:37 +0000

    further changes for multiagent support

--- habitat-baselines/habitat_baselines/rl/ppo/single_agent_access_mgr.py
+++ habitat-baselines/habitat_baselines/rl/ppo/single_agent_access_mgr.py
@@ -107,6 +107,10 @@ class SingleAgentAccessMgr(AgentAccessMgr):
         return (1,)
 
     @property
+    def masks_shape(self):
+        return (1,)
+
+    @property
     def nbuffers(self):
         return self._nbuffers
 
