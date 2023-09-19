1c96298b99023a2d9a14d885769701c83676338a
xavierpuigf <xavierpuigf@gmail.com>
2023-09-19 02:09:07 +0000

    removing unused actions and changes to make policy run

--- habitat-lab/habitat/config/default_structured_configs.py
+++ habitat-lab/habitat/config/default_structured_configs.py
@@ -329,51 +329,6 @@ class OracleNavActionConfig(ActionConfig):
     num_spawn_attempts: int = 200
 
 
-@dataclass
-class OracleNavWithBackingUpActionConfig(ActionConfig):
-    """
-    Rearrangement Only, Oracle navigation action with backing-up motion.
-    This action takes as input a discrete ID which refers to an object in the
-    PDDL domain. The oracle navigation controller then computes the actions to
-    navigate to that desired object.
-    """
-
-    type: str = "OracleNavWithBackingUpAction"
-    # Whether the motion is in the form of base_velocity or human_joints
-    motion_control: str = "base_velocity"
-    num_joints: int = 17
-    turn_velocity: float = 1.0
-    forward_velocity: float = 1.0
-    turn_thresh: float = 0.1
-    dist_thresh: float = 0.2
-    lin_speed: float = 10.0
-    ang_speed: float = 10.0
-    allow_dyn_slide: bool = True
-    allow_back: bool = True
-    # A value of -1.0 means we will get as close to the object as possible.
-    spawn_max_dist_to_obj: float = 2.0
-    num_spawn_attempts: int = 200
-    # For noncylinder navmesh action
-    # The max longitudinal and lateral linear speeds of the robot
-    longitudinal_lin_speed: float = 10.0
-    lateral_lin_speed: float = 10.0
-    # If the condition of sliding includs the checking of rotation
-    enable_rotation_check_for_dyn_slide: bool = True
-    # There is a collision if the difference between the clamped NavMesh position and target position
-    # is more than collision_threshold for any point.
-    collision_threshold: float = 1e-5
-    # If we allow the robot to move laterally.
-    enable_lateral_move: bool = False
-    # The x and y locations of the clamped NavMesh position
-    navmesh_offset: Optional[List[float]] = None
-    # The x and y locations of the clamped NavMesh position for placing and picking locations
-    navmesh_offset_for_agent_placement: Optional[List[float]] = None
-    # Simulation frequency for velocity control
-    sim_freq: float = 120.0
-    # Distance threshold between two agents to issue a stop action
-    agents_dist_thresh: float = -1.0
-
-
 # -----------------------------------------------------------------------------
 # # EQA actions
 # -----------------------------------------------------------------------------
