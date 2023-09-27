9b3bd198af615baa2a6c25412b65e251a962b6b7
aclegg3 <alexanderwclegg@gmail.com>
2023-09-26 17:58:01 -0700

    minor update

--- habitat-lab/habitat/tasks/rearrange/rearrange_task.py
+++ habitat-lab/habitat/tasks/rearrange/rearrange_task.py
@@ -82,8 +82,13 @@ class RearrangeTask(NavigationTask):
         self._episode_id: str = ""
         self._cur_episode_step = 0
         self._should_place_articulated_agent = should_place_articulated_agent
-        # support hab2 benchmark fixed states
-        if self._sim.habitat_config.agents["main_agent"].is_set_start_state:
+        # TODO: this patch supports hab2 benchmark fixed states, but should be refactored w/ state caching for multi-agent
+        if (
+            hasattr(self._sim.habitat_config.agents, "main_agent")
+            and self._sim.habitat_config.agents[
+                "main_agent"
+            ].is_set_start_state
+        ):
             self._should_place_articulated_agent = False
 
         # Get config options
