1ba5144965f33c5205eb1d0f4af5fb34752fd01a
ASzot <ASzot@users.noreply.github.com>
2023-10-09 10:55:22 -0700

    Planner policy

--- habitat-baselines/habitat_baselines/rl/hrl/hl/__init__.py
+++ habitat-baselines/habitat_baselines/rl/hrl/hl/__init__.py
@@ -1,5 +1,11 @@
 from habitat_baselines.rl.hrl.hl.fixed_policy import FixedHighLevelPolicy
 from habitat_baselines.rl.hrl.hl.high_level_policy import HighLevelPolicy
 from habitat_baselines.rl.hrl.hl.neural_policy import NeuralHighLevelPolicy
+from habitat_baselines.rl.hrl.hl.planner_policy import PlannerHighLevelPolicy
 
-__all__ = ["HighLevelPolicy", "FixedHighLevelPolicy", "NeuralHighLevelPolicy"]
+__all__ = [
+    "HighLevelPolicy",
+    "FixedHighLevelPolicy",
+    "NeuralHighLevelPolicy",
+    "PlannerHighLevelPolicy",
+]
