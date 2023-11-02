10ae9c3da249c838f5032ebda121165fc74cc879
rpartsey <partsey2412@gmail.com>
2023-11-02 17:43:26 +0200

    fix EpisodeHelper class

--- examples/siro_sandbox/episode_helper.py
+++ examples/siro_sandbox/episode_helper.py
@@ -2,8 +2,17 @@ class EpisodeHelper:
     def __init__(self, habitat_env):
         self._habitat_env = habitat_env
 
-        self._num_iter_episodes = float("inf") if habitat_env.episode_iterator.cycle else len(self._habitat_env.episode_iterator.episodes)  # type: ignore
+        self._episode_iterator_cycle = habitat_env.episode_iterator.cycle
+        self._num_iter_episodes: int = len(habitat_env.episode_iterator.episodes)  # type: ignore
         self._num_episodes_done: int = 0
+
+    @property
+    def num_iter_episodes(self):
+        return (
+            self._num_iter_episodes
+            if not self._episode_iterator_cycle
+            else float("inf")
+        )
 
     @property
     def num_episodes_done(self):
@@ -17,8 +26,8 @@ class EpisodeHelper:
         self._habitat_env.episode_iterator.set_next_episode_by_id(episode_id)
 
     def next_episode_exists(self):
-        return self._num_episodes_done < self._num_iter_episodes - 1
+        return self.num_episodes_done < self.num_iter_episodes - 1
 
     def increment_done_episode_counter(self):
         self._num_episodes_done += 1
-        assert self._num_episodes_done <= self._num_iter_episodes
+        assert self.num_episodes_done <= self.num_iter_episodes
