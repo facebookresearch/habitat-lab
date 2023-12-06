b59784cddbf6b9a2957437e4ab42983f1245b9d3
Jimmy Yang <yangtsungyen@gmail.com>
2023-12-06 00:23:29 +0000

    address

--- habitat-lab/habitat/config/default_structured_configs.py
+++ habitat-lab/habitat/config/default_structured_configs.py
@@ -1144,7 +1144,7 @@ class RearrangePickRewardMeasurementConfig(MeasurementConfig):
     :property drop_obj_should_end: If true, the task will end if the robot drops the object.
     :property wrong_pick_should_end: If true, the task will end if the robot picks the wrong object.
     :property max_target_distance: default: -1. If it is positive, then we terminate the episode if the robot distance to object is above this value.
-    :property too_far_away_pen: If the robot is too far away, then we terminate the episode by giving the penality.
+    :property max_target_distance_pen: If the robot is too far away, then we terminate the episode by giving the penality.
     :property non_desire_ee_local_pos_dis: default: -1. If positive, we terminate the episode if the robot moves the arm below this threshold
     :property non_desire_ee_local_pos_pen: If the robot moves the arm there, then we terminate the episode by giving the penality.
     :property non_desire_ee_local_pos: If given, we do not want the robot to move the arm there
@@ -1167,7 +1167,7 @@ class RearrangePickRewardMeasurementConfig(MeasurementConfig):
     max_count_colls: int = -1
     count_coll_end_pen: float = 1.0
     max_target_distance: float = -1.0
-    too_far_away_pen: float = 1.0
+    max_target_distance_pen: float = 1.0
     non_desire_ee_local_pos_dis: float = -1.0
     non_desire_ee_local_pos_pen: float = 1.0
     non_desire_ee_local_pos: Optional[List[float]] = None
