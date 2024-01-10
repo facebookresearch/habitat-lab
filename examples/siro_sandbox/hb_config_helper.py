d549696f6ce4bd7c66a0eacbd9c035f9dd8e2938
Eric Undersander <eundersander@meta.com>
2024-01-10 01:50:52 -0600

    add missing copyright headers

--- examples/siro_sandbox/hb_config_helper.py
+++ examples/siro_sandbox/hb_config_helper.py
@@ -1,4 +1,9 @@
-# from habitat_baselines.config.default import get_config as get_baselines_config
+#!/usr/bin/env python3
+
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import habitat
 from habitat.config.default import get_agent_config
 from habitat.config.default_structured_configs import (
--- examples/siro_sandbox/hydra_helper.py
+++ examples/siro_sandbox/hydra_helper.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from habitat.config.default_structured_configs import (
     HabitatConfigPlugin,
     register_hydra_plugin,
