1e066fb3f49f1af3bb70f5506510b91fcc9fc6d7
Xavi Puig <xavierpuig@fb.com>
2024-01-12 16:40:03 -0800

    Add tutorial

--- habitat-lab/habitat/tasks/rearrange/actions/oracle_nav_action.py
+++ habitat-lab/habitat/tasks/rearrange/actions/oracle_nav_action.py
@@ -261,6 +261,253 @@ class OracleNavAction(BaseVelAction, HumanoidJointAction):
                 )
 
 
+@registry.register_task_action
+class OracleNavCoordinateAction(BaseVelAction, HumanoidJointAction):  # type: ignore
+    """
+    An action to drive the anget to a given coordinate
+    """
+
+    def __init__(self, *args, task, **kwargs):
+        config = kwargs["config"]
+        self.motion_type = config.motion_control
+        self._targets = {}
+        if self.motion_type == "base_velocity":
+            BaseVelAction.__init__(self, *args, **kwargs)
+
+        elif self.motion_type == "human_joints":
+            HumanoidJointAction.__init__(self, *args, **kwargs)
+            self.humanoid_controller = self.lazy_inst_humanoid_controller(
+                task, config
+            )
+
+        else:
+            raise ValueError("Unrecognized motion type for oracle nav  action")
+
+    @staticmethod
+    def _compute_turn(rel, turn_vel, robot_forward):
+        is_left = np.cross(robot_forward, rel) > 0
+        if is_left:
+            vel = [0, -turn_vel]
+        else:
+            vel = [0, turn_vel]
+        return vel
+
+    def lazy_inst_humanoid_controller(self, task, config):
+        # Lazy instantiation of humanoid controller
+        # We assign the task with the humanoid controller, so that multiple actions can
+        # use it.
+
+        if (
+            not hasattr(task, "humanoid_controller")
+            or task.humanoid_controller is None
+        ):
+            # Initialize humanoid controller
+            agent_name = self._sim.habitat_config.agents_order[
+                self._agent_index
+            ]
+            walk_pose_path = self._sim.habitat_config.agents[
+                agent_name
+            ].motion_data_path
+
+            humanoid_controller = HumanoidRearrangeController(walk_pose_path)
+            humanoid_controller.set_framerate_for_linspeed(
+                config["lin_speed"], config["ang_speed"], self._sim.ctrl_freq
+            )
+            task.humanoid_controller = humanoid_controller
+        return task.humanoid_controller
+
+    def _update_controller_to_navmesh(self):
+        base_offset = self.cur_articulated_agent.params.base_offset
+        prev_query_pos = self.cur_articulated_agent.base_pos
+        target_query_pos = (
+            self.humanoid_controller.obj_transform_base.translation
+            + base_offset
+        )
+
+        filtered_query_pos = self._sim.step_filter(
+            prev_query_pos, target_query_pos
+        )
+        fixup = filtered_query_pos - target_query_pos
+        self.humanoid_controller.obj_transform_base.translation += fixup
+
+    @property
+    def action_space(self):
+        return spaces.Dict(
+            {
+                self._action_arg_prefix
+                + "oracle_nav_coord_action": spaces.Box(
+                    shape=(3,),
+                    low=np.finfo(np.float32).min,
+                    high=np.finfo(np.float32).max,
+                    dtype=np.float32,
+                ),
+                self._action_arg_prefix
+                + "oracle_nav_lookat_action": spaces.Box(
+                    shape=(3,),
+                    low=np.finfo(np.float32).min,
+                    high=np.finfo(np.float32).max,
+                    dtype=np.float32,
+                ),
+                self._action_arg_prefix + "mode": spaces.Discrete(3),
+            }
+        )
+
+    def _get_target_for_coord(self, look_at_pos, agent_pos, mode=0):
+        """Given a place to look at, selects an agent_pos to navigate"""
+        if mode == 0:
+            return (agent_pos, np.array(look_at_pos))
+        if mode == 2:
+            return (agent_pos, np.array(agent_pos))
+        precision = 0.25
+        pos_key = np.around(look_at_pos / precision, decimals=0) * precision
+        pos_key = tuple(pos_key)
+
+        if pos_key not in self._targets:
+            agent_pos, _, _ = place_agent_at_dist_from_pos(
+                np.array(look_at_pos),
+                0.0,
+                self._config.spawn_max_dist_to_obj,
+                self._sim,
+                self._config.num_spawn_attempts,
+                True,
+                self.cur_articulated_agent,
+            )
+            self._targets[pos_key] = agent_pos
+        else:
+            agent_pos = self._targets[pos_key]
+        if self.motion_type == "human_joints":
+            self.humanoid_controller.reset(
+                self.cur_articulated_agent.base_transformation
+            )
+        return (agent_pos, np.array(look_at_pos))
+
+    def _path_to_point(self, point):
+        """
+        Obtain path to reach the coordinate point. If agent_pos is not given
+        the path starts at the agent base pos, otherwise it starts at the agent_pos
+        value
+        :param point: Vector3 indicating the target point
+        """
+        agent_pos = self.cur_articulated_agent.base_pos
+
+        path = habitat_sim.ShortestPath()
+        path.requested_start = agent_pos
+        path.requested_end = point
+        found_path = self._sim.pathfinder.find_path(path)
+        if not found_path:
+            return [agent_pos, point]
+        return path.points
+
+    def step(self, *args, **kwargs):
+        # mode = 0,1,2
+        # 0 means both target and lookat are given, 1 means only lookat is given, 2 means only target is given
+        self.skill_done = False
+        nav_to_target_coord = kwargs.get(
+            self._action_arg_prefix + "oracle_nav_lookat_action",
+        )
+        nav_position_coord = kwargs.get(
+            self._action_arg_prefix + "oracle_nav_coord_action",
+        )
+        mode = kwargs.get(self._action_arg_prefix + "mode")
+        if np.linalg.norm(nav_to_target_coord) == 0:
+            return {}
+
+        final_nav_targ, obj_targ_pos = self._get_target_for_coord(
+            nav_to_target_coord, nav_position_coord, mode
+        )
+
+        base_T = self.cur_articulated_agent.base_transformation
+        curr_path_points = self._path_to_point(final_nav_targ)
+        robot_pos = np.array(self.cur_articulated_agent.base_pos)
+        if curr_path_points is None:
+            raise Exception
+        else:
+            # Compute distance and angle to target
+            if len(curr_path_points) == 1:
+                curr_path_points += curr_path_points
+            cur_nav_targ = curr_path_points[1]
+            forward = np.array([1.0, 0, 0])
+            robot_forward = np.array(base_T.transform_vector(forward))
+
+            # Compute relative target.
+            rel_targ = cur_nav_targ - robot_pos
+
+            # Compute heading angle (2D calculation)
+            robot_forward = robot_forward[[0, 2]]
+            rel_targ = rel_targ[[0, 2]]
+            rel_pos = (obj_targ_pos - robot_pos)[[0, 2]]
+
+            angle_to_target = get_angle(robot_forward, rel_targ)
+            angle_to_obj = get_angle(robot_forward, rel_pos)
+
+            dist_to_final_nav_targ = np.linalg.norm(
+                (final_nav_targ - robot_pos)[[0, 2]]
+            )
+            at_goal = (
+                dist_to_final_nav_targ < self._config.dist_thresh
+                and angle_to_obj < self._config.turn_thresh
+            ) or dist_to_final_nav_targ < self._config.dist_thresh / 10.0
+
+            if self.motion_type == "base_velocity":
+                if not at_goal:
+                    if dist_to_final_nav_targ < self._config.dist_thresh:
+                        # Look at the object
+                        vel = OracleNavAction._compute_turn(
+                            rel_pos,
+                            self._config.turn_velocity,
+                            robot_forward,
+                        )
+                    elif angle_to_target < self._config.turn_thresh:
+                        # Move towards the target
+                        vel = [self._config.forward_velocity, 0]
+                    else:
+                        # Look at the target waypoint.
+                        vel = OracleNavAction._compute_turn(
+                            rel_targ,
+                            self._config.turn_velocity,
+                            robot_forward,
+                        )
+                else:
+                    vel = [0, 0]
+                kwargs[f"{self._action_arg_prefix}base_vel"] = np.array(vel)
+                return BaseVelAction.step(self, *args, **kwargs)
+
+            elif self.motion_type == "human_joints":
+                # Update the humanoid base
+                self.humanoid_controller.obj_transform_base = base_T
+                if not at_goal:
+                    if dist_to_final_nav_targ < self._config.dist_thresh:
+                        # Look at the object
+                        self.humanoid_controller.calculate_turn_pose(
+                            mn.Vector3([rel_pos[0], 0.0, rel_pos[1]])
+                        )
+                    else:
+                        # Move towards the target
+                        if self._config["lin_speed"] == 0:
+                            distance_multiplier = 0.0
+                        else:
+                            distance_multiplier = 1.0
+                        self.humanoid_controller.calculate_walk_pose(
+                            mn.Vector3([rel_targ[0], 0.0, rel_targ[1]]),
+                            distance_multiplier,
+                        )
+                else:
+                    self.humanoid_controller.calculate_stop_pose()
+                    self.skill_done = True
+                # This line is important to reset the controller
+                self._update_controller_to_navmesh()
+                base_action = self.humanoid_controller.get_pose()
+                kwargs[
+                    f"{self._action_arg_prefix}human_joints_trans"
+                ] = base_action
+
+                return HumanoidJointAction.step(self, *args, **kwargs)
+            else:
+                raise ValueError(
+                    "Unrecognized motion type for oracle nav action"
+                )
+
+
 class SimpleVelocityControlEnv:
     """
     Simple velocity control environment for moving agent
