a259d2ce501eaf70a88a51dd44a8b94ba48262a1
Jimmy Yang <jimmytyyang@meta.com>
2024-02-28 23:18:08 -0500

    add noise for joint

--- habitat-lab/habitat/config/default_structured_configs.py
+++ habitat-lab/habitat/config/default_structured_configs.py
@@ -1633,6 +1633,7 @@ class TaskConfig(HabitatBaseConfig):
     top_down_grasp_ratio: float = 0.5
     fix_obj_rotation_change_arm_joint: bool = False
     joint_start_noise: float = 0.1
+    joint_start_noise_multiplier: Optional[List[int]] = None
 
 
 @dataclass
