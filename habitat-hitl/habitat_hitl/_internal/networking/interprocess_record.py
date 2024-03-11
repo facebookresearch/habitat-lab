00219f9dfc34d5c56909f48bc51694c8a363481b
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-03-11 19:48:34 -0400

    Add interprocess record docstrings.

--- habitat-hitl/habitat_hitl/_internal/networking/interprocess_record.py
+++ habitat-hitl/habitat_hitl/_internal/networking/interprocess_record.py
@@ -11,6 +11,10 @@ from typing import Any, List, Optional
 
 
 class InterprocessRecord:
+    """
+    Utility that stores incoming (client state) and outgoing (keyframe) data such as it can be used by concurrent threads.
+    """
+
     def __init__(self, networking_config, max_steps_ahead: int) -> None:
         self._networking_config = networking_config
         self._keyframe_queue: Queue = Queue()
@@ -19,19 +23,23 @@ class InterprocessRecord:
         self._connection_record_queue: Queue = Queue()
 
     def send_keyframe_to_networking_thread(self, keyframe) -> None:
+        """Send a keyframe (outgoing data) to the networking thread."""
         # Acquire the semaphore to ensure the simulation doesn't advance too far ahead
         self._step_semaphore.acquire()
         self._keyframe_queue.put(keyframe)
 
     def send_client_state_to_main_thread(self, client_state) -> None:
+        """Send a client state (incoming data) to the main thread."""
         self._client_state_queue.put(client_state)
 
     def send_connection_record_to_main_thread(self, connection_record) -> None:
+        """Send a connection record to the main thread."""
         assert "connectionId" in connection_record
         assert "isClientReady" in connection_record
         self._connection_record_queue.put(connection_record)
 
-    def get_queued_keyframes(self) -> List:
+    def get_queued_keyframes(self) -> List[Any]:
+        """Dequeue all keyframes."""
         keyframes = []
 
         while not self._keyframe_queue.empty():
@@ -42,6 +50,7 @@ class InterprocessRecord:
         return keyframes
 
     def get_single_queued_keyframe(self) -> Optional[Any]:
+        """Dequeue one keyframe."""
         if self._keyframe_queue.empty():
             return None
 
@@ -50,7 +59,8 @@ class InterprocessRecord:
         return keyframe
 
     @staticmethod
-    def _dequeue_all(queue: Queue) -> List:
+    def _dequeue_all(queue: Queue) -> List[Any]:
+        """Dequeue all items from a queue."""
         items = []
 
         while not queue.empty():
@@ -60,7 +70,9 @@ class InterprocessRecord:
         return items
 
     def get_queued_client_states(self) -> List:
+        """Dequeue all client states."""
         return self._dequeue_all(self._client_state_queue)
 
     def get_queued_connection_records(self) -> List:
+        """Dequeue all connection records."""
         return self._dequeue_all(self._connection_record_queue)
