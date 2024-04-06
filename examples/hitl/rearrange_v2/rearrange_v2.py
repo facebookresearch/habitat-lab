158d740d89f8c963d64b3d0a39e378732d126836
Ram81 <ramramrakhya81@gmail.com>
2024-04-05 20:45:47 -0400

    [Dataset] Update language rearrange HITL app config to use CollaborationDataset

--- examples/hitl/rearrange_v2/rearrange_v2.py
+++ examples/hitl/rearrange_v2/rearrange_v2.py
@@ -9,7 +9,7 @@ from typing import Any, Dict, Set
 
 import hydra
 import magnum as mn
-from habitat_llm.agent.env import dataset
+from habitat_llm.agent.env import dataset  # noqa: F401
 
 import habitat_sim
 from habitat.sims.habitat_simulator import sim_utilities
@@ -134,7 +134,9 @@ class AppStateRearrangeV2(AppState):
         # TODO: Caching
         # TODO: Improve heuristic using bounding box sizes and view angle
         for handle, _ in self._ao_root_bbs.items():
-            ao = self.get_sim_utilities().get_obj_from_handle(self._sim, handle)
+            ao = self.get_sim_utilities().get_obj_from_handle(
+                self._sim, handle
+            )
             ao_pos = ao.translation
             ao_pos_xz = mn.Vector3(ao_pos.x, 0.0, ao_pos.z)
             dist_xz = (ao_pos_xz - player_pos_xz).length()
@@ -283,13 +285,9 @@ class AppStateRearrangeV2(AppState):
         controls_str: str = ""
         if not self._hide_gui_text:
             if self._sps_tracker.get_smoothed_rate() is not None:
-                controls_str += (
-                    f"server SPS: {self._sps_tracker.get_smoothed_rate():.1f}\n"
-                )
+                controls_str += f"server SPS: {self._sps_tracker.get_smoothed_rate():.1f}\n"
             if self._client_helper and self._client_helper.display_latency_ms:
-                controls_str += (
-                    f"latency: {self._client_helper.display_latency_ms:.0f}ms\n"
-                )
+                controls_str += f"latency: {self._client_helper.display_latency_ms:.0f}ms\n"
             controls_str += "H: show/hide help text\n"
             controls_str += "P: pause\n"
             controls_str += "I, K: look up, down\n"
@@ -442,7 +440,9 @@ class AppStateRearrangeV2(AppState):
         self._update_help_text()
 
 
-@hydra.main(version_base=None, config_path="config", config_name="rearrange_v2")
+@hydra.main(
+    version_base=None, config_path="config", config_name="rearrange_v2"
+)
 def main(config):
     hitl_main(
         config,
