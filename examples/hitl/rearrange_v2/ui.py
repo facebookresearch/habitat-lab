615a6c1484f60ec8c2bed4b0e36b31c841c94653
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-04-25 19:31:04 -0400

    Fix wrong open/close labels.

--- examples/hitl/rearrange_v2/ui.py
+++ examples/hitl/rearrange_v2/ui.py
@@ -255,7 +255,7 @@ class UI:
 
                         self._events.append(
                             {
-                                "type": "open",
+                                "type": "close",
                                 "obj_handle": ao.handle,
                                 "obj_id": object_id,
                             }
@@ -266,7 +266,7 @@ class UI:
 
                         self._events.append(
                             {
-                                "type": "close",
+                                "type": "open",
                                 "obj_handle": ao.handle,
                                 "obj_id": object_id,
                             }
