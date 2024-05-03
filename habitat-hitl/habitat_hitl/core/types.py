15c01361fdf70672ec772c37edf3c565437ff769
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-05-03 10:02:38 -0400

    Add multiplayer support to networking process.

--- habitat-hitl/habitat_hitl/core/types.py
+++ habitat-hitl/habitat_hitl/core/types.py
@@ -4,13 +4,23 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Any, Dict
+from dataclasses import dataclass
+from typing import Any, Dict, List
 
 # Dictionary that is serialized to or from JSON.
 DataDict = Dict[str, Any]
 
 # Server -> Client communication dictionary originating from habitat-sim (loads, updates, deletions, ...).
 Keyframe = DataDict
+
+# Server -> Client communication dictionary that is user specific and added to keyframes before sending the payload to the user (keyframe["message"]).
+Message = DataDict
+
+# Keyframe and all user messages for a specific frame.
+@dataclass
+class KeyframeAndMessages:
+    keyframe: Keyframe
+    messages: List[Message]
 
 # Client -> Server communication dictionary (inputs, etc.).
 ClientState = DataDict
