11ec7bcaf2436be103e995b588d99d16b6911e20
Mikaël Dallaire Côté <110583667+0mdc@users.noreply.github.com>
2024-05-20 14:58:26 -0400

    HITL - Rearrange session handling (#1965)
    * Add session management.
    
    * Formatting changes.
    
    * Add clarifications to episode resolution.
    
    * Document temporary hack to check for client-side loading status.
    
    * Review pass - variable renaming and camera matrix chaching.

--- examples/hitl/rearrange_v2/app_states.py
+++ examples/hitl/rearrange_v2/app_states.py
@@ -10,6 +10,7 @@ Boilerplate code for creating states without circular dependencies.
 
 from app_data import AppData
 from app_state_base import AppStateBase
+from session import Session
 
 from habitat_hitl.app_states.app_service import AppService
 
@@ -30,9 +31,50 @@ def create_app_state_lobby(
     return AppStateLobby(app_service, app_data)
 
 
-def create_app_state_rearrange(
+def create_app_state_start_session(
     app_service: AppService, app_data: AppData
+) -> AppStateBase:
+    from app_state_start_session import AppStateStartSession
+
+    return AppStateStartSession(app_service, app_data)
+
+
+def create_app_state_load_episode(
+    app_service: AppService, app_data: AppData, session: Session
+) -> AppStateBase:
+    from app_state_load_episode import AppStateLoadEpisode
+
+    return AppStateLoadEpisode(app_service, app_data, session)
+
+
+def create_app_state_start_screen(
+    app_service: AppService, app_data: AppData, session: Session
+) -> AppStateBase:
+    from app_state_start_screen import AppStateStartScreen
+
+    return AppStateStartScreen(app_service, app_data, session)
+
+
+def create_app_state_rearrange(
+    app_service: AppService, app_data: AppData, session: Session
 ) -> AppStateBase:
     from rearrange_v2 import AppStateRearrangeV2
 
-    return AppStateRearrangeV2(app_service, app_data)
+    return AppStateRearrangeV2(app_service, app_data, session)
+
+
+def create_app_state_end_session(
+    app_service: AppService, app_data: AppData, session: Session
+) -> AppStateBase:
+    from app_state_end_session import AppStateEndSession
+
+    return AppStateEndSession(app_service, app_data, session)
+
+
+def create_app_state_cancel_session(
+    app_service: AppService, app_data: AppData, session: Session, error: str
+) -> AppStateBase:
+    from app_state_end_session import AppStateEndSession
+
+    session.error = error
+    return AppStateEndSession(app_service, app_data, session)
