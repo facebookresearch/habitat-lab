f42332df2ee2ab230732ab4eaf45cb69756edbad
trandaniel <trandaniel@meta.com>
2024-07-06 12:16:19 -0700

    Update docstrings.

--- habitat-lab/habitat/core/env.py
+++ habitat-lab/habitat/core/env.py
@@ -3,7 +3,7 @@
 # Copyright (c) Meta Platforms, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
+"""TODO: MISSING MODULE DESCRIPTION"""
 import random
 import time
 from typing import (
