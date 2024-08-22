53f9ddad40d6f65c1353dd17d243ba7763443d72
aclegg3 <alexanderwclegg@gmail.com>
2024-08-22 08:51:37 -0700

    add docs

--- habitat-lab/habitat/core/challenge.py
+++ habitat-lab/habitat/core/challenge.py
@@ -11,6 +11,10 @@ from habitat.core.logging import logger
 
 
 class Challenge(Benchmark):
+    """
+    Extends the Benchmark class to run evaluate the current challenge config and submit results to the remote evaluation server.
+    """
+
     def __init__(self, eval_remote=False):
         config_paths = os.environ["CHALLENGE_CONFIG_FILE"]
         super().__init__(config_paths, eval_remote=eval_remote)
