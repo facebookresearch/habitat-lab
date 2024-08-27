60fbc909f8c102851760a78e5e3dd3946b461c23
Mikaël Dallaire Côté <110583667+0mdc@users.noreply.github.com>
2024-08-26 20:38:13 -0400

    HITL - Add object states and categories. (#2035)
    * Add object states and categories to HITL.
    
    * Disable placeholder prints.
    
    * Enable receptacle filtering in MetadataInterface initialization.

--- examples/hitl/rearrange_v2/world.py
+++ examples/hitl/rearrange_v2/world.py
@@ -4,9 +4,18 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from __future__ import annotations
+
+from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Set, Union
 
+from omegaconf import DictConfig
+
 from habitat.sims.habitat_simulator import sim_utilities
+from habitat.sims.habitat_simulator.object_state_machine import (
+    ObjectStateMachine,
+    ObjectStateSpec,
+)
 from habitat.tasks.rearrange.articulated_agent_manager import (
     ArticulatedAgentManager,
 )
@@ -18,6 +27,12 @@ from habitat_sim.physics import (
 from habitat_sim.scene import SemanticRegion
 
 
+@dataclass
+class ObjectStateInfo:
+    state_spec: ObjectStateSpec
+    value: Any
+
+
 class World:
     """
     Global world state shared by each user.
@@ -27,8 +42,10 @@ class World:
     def __init__(
         self,
         sim: RearrangeSim,
+        config: DictConfig,
     ):
         self._sim = sim
+        self._config = config
 
         # Cache of all held objects.
         # Prevents users from picking objects held by others.
@@ -46,6 +63,16 @@ class World:
         self._agent_object_ids: Set[int] = set()
         # Cache of region names.
         self._regions: Dict[int, SemanticRegion] = {}
+        # Cache of all active states.
+        self._all_states: Optional[Dict[str, ObjectStateSpec]] = None
+        # Cache of object states.
+        self._state_snapshot_dict: Dict[str, Dict[str, Any]] = {}
+        # Object state container.
+        self._object_state_machine: Optional[ObjectStateMachine] = None
+        # Cache of categories for each object handles.
+        self._object_handle_categories: Dict[str, Optional[str]] = {}
+        # Interface for handling scene metadata.
+        self._metadata_interface: Optional[Any] = None
 
     def reset(self) -> None:
         """
@@ -96,6 +123,102 @@ class World:
                         link_index = self.get_link_index(link_object_id)
                         if link_index:
                             sim_utilities.close_link(ao, link_index)
+
+        # Object id <-> handle mapping
+        self._id_to_handle: Dict[int, str] = {}
+        self._handle_to_id: Dict[str, int] = {}
+        all_objects = sim_utilities.get_all_objects(sim)
+        for obj in all_objects:
+            self._id_to_handle[obj.object_id] = obj.handle
+            self._handle_to_id[obj.handle] = obj.object_id
+
+        # Try initializing object state and metadata information.
+        self._try_initialize_object_states(sim, self._config)
+
+    def update(self, dt: float) -> None:
+        """
+        Update the world state. Call every frame.
+        """
+        sim = self._sim
+        osm = self._object_state_machine
+
+        if osm is not None:
+            osm.update_states(sim, dt)
+            self._state_snapshot_dict = osm.get_snapshot_dict(sim)
+
+    def get_state_snapshot_dict(self) -> Dict[str, Dict[str, Any]]:
+        """
+        Get a snapshot of the current state of all objects.
+
+        Example:
+        {
+            "is_powered_on": {
+                "my_lamp.0001": True,
+                "my_oven": False,
+                ...
+            },
+            "is_clean": {
+                "my_dish.0002:" False,
+                ...
+            },
+            ...
+        }
+        """
+        return self._state_snapshot_dict
+
+    def get_all_states(self) -> Dict[str, ObjectStateSpec]:
+        """
+        Get a map of all object states active in this world.
+        The key is the state name, and the value is the state specification.
+        """
+        return self._all_states
+
+    def get_states_for_object_handle(
+        self, object_handle: str
+    ) -> List[ObjectStateInfo]:
+        """
+        Get a list of all object states for a given object handle.
+        """
+        object_states: Dict[str, Any] = {}
+        state_snapshot = self.get_state_snapshot_dict()
+
+        for state_id, handles in state_snapshot.items():
+            if object_handle in handles:
+                object_states[state_id] = handles[object_handle]
+
+        state_infos: List[ObjectStateInfo] = []
+        all_object_states = self.get_all_states()
+        for state, value in object_states.items():
+            state_spec = all_object_states[state]
+            state_infos.append(
+                ObjectStateInfo(
+                    state_spec=state_spec,
+                    value=value,
+                )
+            )
+
+        return state_infos
+
+    def get_category_from_handle(self, object_handle: str) -> Optional[str]:
+        """
+        Get the semantic category name of a given object handle.
+        Returns None if the object category is unknown.
+        """
+        return self._object_handle_categories.get(object_handle, None)
+
+    def handle_to_id(self, handle: str) -> Optional[int]:
+        """
+        Get the object_id associated to a given object handle.
+        Note that for articulated objects, all children (links) have different IDs but the same handle.
+        """
+        return self._handle_to_id.get(handle, None)
+
+    def id_to_handle(self, object_id: int) -> Optional[str]:
+        """
+        Get the object handle associated to a given object id.
+        Note that for articulated objects, all children (links) have different IDs but the same handle.
+        """
+        return self._id_to_handle.get(object_id, None)
 
     def get_rigid_object(
         self, object_id: int
@@ -165,3 +288,47 @@ class World:
                 return True
 
         return object_id in self._all_held_object_ids
+
+    def _try_initialize_object_states(
+        self, sim: RearrangeSim, config: DictConfig
+    ) -> None:
+        """
+        Try initializing object states and categories. Depends on the 'habitat_llm' external package.
+        """
+        try:
+            # Initialize object state machine.
+            from habitat_llm.sims.collaboration_sim import (
+                initialize_object_state_machine,
+            )
+            from habitat_llm.sims.metadata_interface import (
+                MetadataInterface,
+                get_metadata_dict_from_config,
+            )
+
+            metadata_dict = get_metadata_dict_from_config(
+                dataset_config=config.habitat.dataset
+            )
+            metadata_interface = MetadataInterface(metadata_dict)
+            metadata_interface.refresh_scene_caches(
+                sim, filter_receptacles=True
+            )
+            self._metadata_interface = metadata_interface
+            osm = initialize_object_state_machine(sim, metadata_interface)
+            self._object_state_machine = osm
+
+            # Get object semantic categories.
+            all_objects = sim_utilities.get_all_objects(sim)
+            for obj in all_objects:
+                self._object_handle_categories[
+                    obj.handle
+                ] = metadata_interface.get_object_instance_category(obj)
+
+            # Get active object states.
+            active_states = osm.active_states
+            active_states_dict: Dict[str, ObjectStateSpec] = {}
+            for active_state in active_states:
+                active_states_dict[active_state.name] = active_state
+            self._all_states = active_states_dict
+        except Exception as e:
+            print(f"Object states could not be loaded. {e}")
+            self._object_state_machine = None
