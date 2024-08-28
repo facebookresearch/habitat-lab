5629593ee4dbcd1621107567b3ca31bb0b4a5520
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-08-28 16:08:24 -0400

    Comment and doctstring adjustments.

--- examples/hitl/rearrange_v2/object_state_manipulator.py
+++ examples/hitl/rearrange_v2/object_state_manipulator.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Tuple, cast
 
@@ -68,6 +74,17 @@ class ObjectStateManipulator:
                 False: OraclePowerOffInPlaceSkill,
             },
         }
+        """
+        Map of boolean state values and their associated skill.
+
+        Example:
+        ```
+        "is_powered_on": {
+            True: PowerOnSkill,
+            False: PowerOffSkill,
+        },
+        ```
+        """
 
     def set_object_state(
         self, object_handle: str, state_name: str, state_value: Any
