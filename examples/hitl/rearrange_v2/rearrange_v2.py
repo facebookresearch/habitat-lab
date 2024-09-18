06585137e3ae22b0cebc4c813c12b701f84c5551
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-09-17 22:05:10 -0400

    Add metrics to HITL output.

--- examples/hitl/rearrange_v2/rearrange_v2.py
+++ examples/hitl/rearrange_v2/rearrange_v2.py
@@ -728,7 +728,7 @@ class AppStateRearrangeV2(AppStateBase):
         super().on_exit()
 
         task_percent_complete = self._metrics.get_task_percent_complete()
-        feedback = self._metrics.get_task_explanation()
+        metrics = self._metrics.get_all_metrics()
 
         episode_finished = self._is_episode_finished() and not self._cancel
 
@@ -737,7 +737,7 @@ class AppStateRearrangeV2(AppStateBase):
             task_percent_complete=task_percent_complete
             if task_percent_complete is not None
             else 1.0,
-            task_explanation=feedback,
+            metrics=metrics,
         )
 
         for user_data in self._user_data:
