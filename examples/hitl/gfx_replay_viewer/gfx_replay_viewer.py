b621afba9d0a852214a07f48caebc7704b410c7f
eundersander <eundersander@meta.com>
2024-09-24 02:12:54 -0500

    gfx_replay_viewer polish

--- examples/hitl/gfx_replay_viewer/gfx_replay_viewer.py
+++ examples/hitl/gfx_replay_viewer/gfx_replay_viewer.py
@@ -6,6 +6,7 @@
 
 import hydra
 import magnum as mn
+import json
 
 from habitat_hitl.app_states.app_service import AppService
 from habitat_hitl.app_states.app_state_abc import AppState
@@ -47,20 +48,96 @@ class AppStateGfxReplayViewer(AppState):
         # not supported for pick_throw_vr
         assert not self._app_service.hitl_config.camera.first_person_mode
 
-        self._camera_lookat_pos = mn.Vector3(0, 0, 0)
+        self._camera_lookat_base_pos = mn.Vector3(0, 0, 0)
+        self._camera_lookat_y_offset = 1.0
         self._camera_helper.update(self._get_camera_lookat_pos(), 0.0)
+
+        self._is_recording = False
+        self._hide_gui = False
+
+        self._camera_follow_key = "agent0"
+
+        with open(self._gfx_replay_viewer_cfg.text_overlays_path, 'r') as f:
+            text_overlays_obj = json.load(f)
+            self._text_overlays = text_overlays_obj["overlays"]
+
+        with open(self._gfx_replay_viewer_cfg.replay_filepath, 'r') as f:
+            gfx_replay_obj = json.load(f)
+            gfx_replay_keyframes = gfx_replay_obj["keyframes"]
+
+        self._num_keyframes = len(gfx_replay_keyframes)
+        self._replay_user_positions = {}
+        for keyframe_idx in range(self._num_keyframes):
+            keyframe_user_transforms = gfx_replay_keyframes[keyframe_idx]["userTransforms"]
+            for user_transform in keyframe_user_transforms:
+                key = user_transform["name"]
+                position = user_transform["transform"]["translation"]
+                if key not in self._replay_user_positions:
+                    assert keyframe_idx == 0
+                    self._replay_user_positions[key] = []
+                self._replay_user_positions[key].append(position)
+
+        for key in self._replay_user_positions:
+            assert len(self._replay_user_positions[key]) == self._num_keyframes
+        
+
+
+    def draw_debug_nav_lines(self):
+
+        agent_colors = [
+            mn.Color3(1, 0.75, 0),
+            mn.Color3(0, 0.5, 1.0)
+        ]
+
+        gui_drawer = self._app_service.gui_drawer
+
+        for agent_id in [0, 1]:
+            points = []
+            agent_key = f"agent{agent_id}"
+            agent_side = "left" if agent_id == 0 else "right"
+            agent_positions = self._replay_user_positions[agent_key]
+
+            end_keyframe = self._num_keyframes
+            for overlay in self._text_overlays:
+                if overlay["frame"] > self._keyframe_index and overlay["side"] == agent_side:
+                    end_keyframe = overlay["frame"] + 1
+                    break
+
+            for keyframe_idx in range(self._keyframe_index, end_keyframe):
+                pos = agent_positions[keyframe_idx]
+                points.append(pos)
+
+            radius = 0.5
+            color = agent_colors[agent_id]
+            if len(points) >= 2:
+                gui_drawer.draw_path_with_endpoint_circles(points, radius, color)
+            elif len(points) >= 1:
+                gui_drawer.draw_circle(points[0], radius, color)
+
 
     def _get_controls_text(self):
         controls_str: str = ""
+        controls_str += "T: toggle camera lookat\n"
+        controls_str += "Y: play and record\n"
+        controls_str += "7: rewind to start\n"
+        controls_str += "8: jump back\n"
+        controls_str += "9: hold to play\n"
+        controls_str += "0: jump forward\n"
         controls_str += "ESC: exit\n"
         return controls_str
 
     def _get_status_text(self):
-        status_str = f"keyframe: {self._keyframe_index}\n"
-        status_str += f"({self._camera_lookat_pos.x:.1f}, {self._camera_lookat_pos.y:.1f}, {self._camera_lookat_pos.z:.1f})\n"
+        status_str = ""
+        status_str += f"keyframe: {self._keyframe_index}\n"
+        status_str += f"camera: {self._camera_follow_key if self._camera_follow_key else 'free'}"
+        lookat_pos = self._get_camera_lookat_pos()
+        status_str += f"({lookat_pos.x:.1f}, {lookat_pos.y:.1f}, {lookat_pos.z:.1f})\n"
         return status_str
 
     def _update_help_text(self):
+        if self._hide_gui:
+            return
+
         controls_str = self._get_controls_text()
         if len(controls_str) > 0:
             self._app_service.text_drawer.add_text(
@@ -76,40 +153,109 @@ class AppStateGfxReplayViewer(AppState):
             )
 
     def _get_camera_lookat_pos(self):
-        return self._camera_lookat_pos
+        return self._camera_lookat_base_pos + mn.Vector3(0, self._camera_lookat_y_offset, 0)
+
+    def _update_camera_lookat_base_pos(self):
+
+        gui_input = self._app_service.gui_input
+        y_speed = 0.05
+        if gui_input.get_key_down(KeyCode.Z):
+            self._camera_lookat_y_offset -= y_speed
+        if gui_input.get_key_down(KeyCode.X):
+            self._camera_lookat_y_offset += y_speed
+
+        if self._camera_follow_key:
+            tuple = self._player.get_user_transform(self._camera_follow_key)
+            if tuple:
+                self._camera_lookat_base_pos = tuple[0]
+        else:
+
+            xz_forward = self._camera_helper.get_xz_forward()
+            xz_right = mn.Vector3(-xz_forward.z, 0.0, xz_forward.x)
+            speed = self._gfx_replay_viewer_cfg.camera_move_speed * self._camera_helper.get_cam_zoom_dist()
+            if gui_input.get_key(KeyCode.W):
+                self._camera_lookat_base_pos += (
+                    xz_forward * speed
+                )
+            if gui_input.get_key(KeyCode.S):
+                self._camera_lookat_base_pos -= (
+                    xz_forward * speed
+                )
+            if gui_input.get_key(KeyCode.E):
+                self._camera_lookat_base_pos += (
+                    xz_right * speed
+                )
+            if gui_input.get_key(KeyCode.Q):
+                self._camera_lookat_base_pos -= (
+                    xz_right * speed
+                )
+
 
     def sim_update(self, dt, post_sim_update_dict):
         gui_input = self._app_service.gui_input
         if gui_input.get_key_down(KeyCode.ESC):
             post_sim_update_dict["application_exit"] = True
 
-        if gui_input.get_key(KeyCode.SPACE):
-            self._keyframe_index += 1
-            if self._keyframe_index == self._player.get_num_keyframes():
-                self._keyframe_index = 0
-            self._player.set_keyframe_index(self._keyframe_index)
+        # for step_size, key_dec, key_inc in [(1, KeyCode.SEVEN, KeyCode.EIGHT), (16, KeyCode.NINE, KeyCode.ZERO)]:
+        #     if gui_input.get_key_down(key_dec) and self._keyframe_index > 0:
+        #         self._keyframe_index = max(self._keyframe_index - step_size, 0)
+        #         self._player.set_keyframe_index(self._keyframe_index)
+        #     if gui_input.get_key(key_inc) and self._keyframe_index < self._player.get_num_keyframes() - 1:
+        #         self._keyframe_index = min(self._keyframe_index + step_size, self._player.get_num_keyframes() - 1)
+        #         self._player.set_keyframe_index(self._keyframe_index)
 
-        xz_forward = self._camera_helper.get_xz_forward()
-        if gui_input.get_key(KeyCode.W):
-            self._camera_lookat_pos += (
-                xz_forward * self._gfx_replay_viewer_cfg.camera_move_speed
-            )
-        if gui_input.get_key(KeyCode.S):
-            self._camera_lookat_pos -= (
-                xz_forward * self._gfx_replay_viewer_cfg.camera_move_speed
-            )
+        self._update_camera_lookat_base_pos()
+
+        if gui_input.get_key_down(KeyCode.T):
+            if self._camera_follow_key == "agent0":
+                self._camera_follow_key = "agent1"
+            elif self._camera_follow_key == "agent1":
+                self._camera_follow_key = None
+            else:
+                self._camera_follow_key = "agent0"
+
+        if self._is_recording:
+            self._keyframe_index += 1
+            reached_end = False
+            if self._keyframe_index == self._player.get_num_keyframes():
+                self._keyframe_index = self._player.get_num_keyframes() - 1
+                reached_end = True
+            self._player.set_keyframe_index(self._keyframe_index)
+            if gui_input.get_key_down(KeyCode.Y) or reached_end:
+                self._app_service.video_recorder.stop_recording_and_save_video()
+                self._is_recording = False
+                self._hide_gui = False
+        else:
+            if gui_input.get_key_down(KeyCode.Y):
+                self._app_service.video_recorder.start_recording()
+                self._is_recording = True
+                self._hide_gui = True
+            else:
+                # seek controls only active when not recording
+                if gui_input.get_key_down(KeyCode.SEVEN):
+                    self._keyframe_index = 0
+                elif gui_input.get_key_down(KeyCode.EIGHT):
+                    self._keyframe_index = max(self._keyframe_index - 30, 0)
+                elif gui_input.get_key(KeyCode.NINE):  # note use of get_key so you can hold this key
+                    self._keyframe_index = min(self._keyframe_index + 1, self._player.get_num_keyframes() - 1)
+                elif gui_input.get_key(KeyCode.ZERO):
+                    self._keyframe_index = min(self._keyframe_index + 8, self._player.get_num_keyframes() - 1)
+                self._player.set_keyframe_index(self._keyframe_index)
 
         self._camera_helper.update(self._get_camera_lookat_pos(), dt)
         self._cam_transform = self._camera_helper.get_cam_transform()
         post_sim_update_dict["cam_transform"] = self._cam_transform
 
-        lookat_ring_radius = 0.1
-        lookat_ring_color = mn.Color3(1, 0.75, 0)
-        self._app_service.gui_drawer.draw_circle(
-            self._camera_lookat_pos,
-            lookat_ring_radius,
-            lookat_ring_color,
-        )
+        if not self._hide_gui:
+            lookat_ring_radius = 0.1
+            lookat_ring_color = mn.Color3(1, 0.75, 0)
+            self._app_service.gui_drawer.draw_circle(
+                self._camera_lookat_base_pos,
+                lookat_ring_radius,
+                lookat_ring_color,
+            )
+
+        self.draw_debug_nav_lines()
 
         self._update_help_text()
 
