52d4e3cba514d084d653ed052b464a4073b99e36
trandaniel <trandaniel@meta.com>
2024-12-10 09:40:39 -0800

    Move Applauncher class into if/main statement.

--- habitat-lab/habitat/isaacsim/converter.py
+++ habitat-lab/habitat/isaacsim/converter.py
@@ -12,10 +12,7 @@ from omni.isaac.lab.app import AppLauncher
 import os
 import timer
 
-#Run laucher through Isaac Lab
 
-
-#async def convert_glb_to_usd(glb_folder_path: str, usd_folder_path: str) -> None:
 def convert_glb_to_usd(glb_folder_path: str, usd_folder_path: str) -> None:
     """
     This function converts glb files to usd. This function launches an empty Isaac Lab stage,
@@ -23,18 +20,6 @@ def convert_glb_to_usd(glb_folder_path: str, usd_folder_path: str) -> None:
     MeshConverter._convert_mesh_to_usd async function. The simulation app then closes, and
     the converted functions are dumped onto the respective usd_folder_path.
     """
-
-    # create argparser
-    parser = argparse.ArgumentParser(description="Tutorial on creating an empty stage.")
-    # append AppLauncher cli args
-    AppLauncher.add_app_launcher_args(parser)
-    # parse the arguments
-    args_cli = parser.parse_args()
-    # launch omniverse app
-    app_launcher = AppLauncher(args_cli)
-    simulation_app = app_launcher.app
-
-    from omni.isaac.lab.sim.converters import MeshConverter #NOTE: Import isaac lab converter extension after launcher runs
 
     glb_folder_list = [glb for glb in os.listdir(glb_folder_path) if os.path.isfile(os.path.join(glb_folder_path, glb))]
 
@@ -52,10 +37,24 @@ def convert_glb_to_usd(glb_folder_path: str, usd_folder_path: str) -> None:
         )
         converter = MeshConverter(converter_config)
         print(f"Wrote {converter.usd_path}"
-              
-    simulation_app.close()
+            
 
 if __name__ == "__main__":
     glb_folder_path = '/path/object_glb/'
     usd_folder_path = '/path/object_usd/'
+    
+    # create argparser
+    parser = argparse.ArgumentParser(description="Tutorial on creating an empty stage.")
+    # append AppLauncher cli args
+    AppLauncher.add_app_launcher_args(parser)
+    # parse the arguments
+    args_cli = parser.parse_args()
+    # launch omniverse app
+    app_launcher = AppLauncher(args_cli)
+    simulation_app = app_launcher.app
+
+    from omni.isaac.lab.sim.converters import MeshConverter #NOTE: Import isaac lab converter extension after launcher runs
+
     convert_glb_to_usd(glb_folder_path, usd_folder_path)
+
+    simulation_app.close()
