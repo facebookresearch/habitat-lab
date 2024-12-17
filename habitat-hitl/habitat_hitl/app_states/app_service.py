14f57434efa98e01035767a5ee36d8ebefed77e5
eundersander <eundersander@meta.com>
2024-12-17 05:07:31 -0600

    add HitlBareSimDriver

--- habitat-hitl/habitat_hitl/app_states/app_service.py
+++ habitat-hitl/habitat_hitl/app_states/app_service.py
@@ -41,6 +41,7 @@ class AppService:
         get_anim_fraction: Callable,
         env: Env,
         sim: RearrangeSim,
+        reconfigure_sim: Callable,
         compute_action_and_step_env: Callable,
         step_recorder: BaseRecorder,
         get_metrics: Callable[[], Dict[str, Any]],
@@ -62,6 +63,7 @@ class AppService:
         self._get_anim_fraction = get_anim_fraction
         self._env = env
         self._sim = sim
+        self._reconfigure_sim = reconfigure_sim
         self._compute_action_and_step_env = compute_action_and_step_env
         self._step_recorder = step_recorder
         self._get_metrics = get_metrics
@@ -115,6 +117,10 @@ class AppService:
     @property
     def sim(self) -> RearrangeSim:
         return self._sim
+    
+    @property
+    def reconfigure_sim(self) -> Callable:
+        return self._reconfigure_sim
 
     @property
     def compute_action_and_step_env(self) -> Callable:
