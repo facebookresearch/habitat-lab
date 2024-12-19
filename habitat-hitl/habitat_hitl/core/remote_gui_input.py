ff422043dac54c1c01e0903a289091e3b5b65df8
eundersander <eundersander@meta.com>
2024-12-18 23:45:39 -0600

    [HITL]: RemoteGuiInput support for articulated hands

--- habitat-hitl/habitat_hitl/core/remote_gui_input.py
+++ habitat-hitl/habitat_hitl/core/remote_gui_input.py
@@ -87,6 +87,75 @@ class RemoteGuiInput:
 
         return pos, rot_quat
 
+
+    def get_articulated_hand_pose(self, hand_idx, history_index=0):
+        """
+        Returns world-space positions and rotations of 26 hand bones.
+        https://registry.khronos.org/OpenXR/specs/1.0/html/xrspec.html#convention-of-hand-joints
+        typedef enum XrHandJointEXT {
+            XR_HAND_JOINT_PALM_EXT = 0,
+            XR_HAND_JOINT_WRIST_EXT = 1,
+            XR_HAND_JOINT_THUMB_METACARPAL_EXT = 2,
+            XR_HAND_JOINT_THUMB_PROXIMAL_EXT = 3,
+            XR_HAND_JOINT_THUMB_DISTAL_EXT = 4,
+            XR_HAND_JOINT_THUMB_TIP_EXT = 5,
+            XR_HAND_JOINT_INDEX_METACARPAL_EXT = 6,
+            XR_HAND_JOINT_INDEX_PROXIMAL_EXT = 7,
+            XR_HAND_JOINT_INDEX_INTERMEDIATE_EXT = 8,
+            XR_HAND_JOINT_INDEX_DISTAL_EXT = 9,
+            XR_HAND_JOINT_INDEX_TIP_EXT = 10,
+            XR_HAND_JOINT_MIDDLE_METACARPAL_EXT = 11,
+            XR_HAND_JOINT_MIDDLE_PROXIMAL_EXT = 12,
+            XR_HAND_JOINT_MIDDLE_INTERMEDIATE_EXT = 13,
+            XR_HAND_JOINT_MIDDLE_DISTAL_EXT = 14,
+            XR_HAND_JOINT_MIDDLE_TIP_EXT = 15,
+            XR_HAND_JOINT_RING_METACARPAL_EXT = 16,
+            XR_HAND_JOINT_RING_PROXIMAL_EXT = 17,
+            XR_HAND_JOINT_RING_INTERMEDIATE_EXT = 18,
+            XR_HAND_JOINT_RING_DISTAL_EXT = 19,
+            XR_HAND_JOINT_RING_TIP_EXT = 20,
+            XR_HAND_JOINT_LITTLE_METACARPAL_EXT = 21,
+            XR_HAND_JOINT_LITTLE_PROXIMAL_EXT = 22,
+            XR_HAND_JOINT_LITTLE_INTERMEDIATE_EXT = 23,
+            XR_HAND_JOINT_LITTLE_DISTAL_EXT = 24,
+            XR_HAND_JOINT_LITTLE_TIP_EXT = 25,
+            XR_HAND_JOINT_MAX_ENUM_EXT = 0x7FFFFFFF
+        } XrHandJointEXT;
+        """        
+        client_state = self.get_recent_client_state_by_history_index(
+            history_index
+        )
+        if not client_state:
+            return None, None
+
+        if "avatar" not in client_state:
+            return None, None
+
+        art_hands_json = client_state["avatar"]["articulatedHands"]
+        assert hand_idx >= 0 and hand_idx < len(art_hands_json)
+
+        art_hand_json = art_hands_json[hand_idx]
+        pos_json = art_hand_json["positions"]
+        num_positions = len(pos_json) // 3
+
+        rot_json = art_hand_json["rotations"]
+        num_rotations = len(rot_json) // 4
+        assert num_positions == num_rotations
+
+        positions = []
+        rotations = []
+        for i in range(num_positions):
+            pos = mn.Vector3(pos_json[i * 3 + 0], pos_json[i * 3 + 1], pos_json[i * 3 + 2])
+            positions.append(pos)
+
+            rot_quat = mn.Quaternion(
+                mn.Vector3(rot_json[i * 3 + 1], rot_json[i * 3 + 2], rot_json[i * 3 + 3]), rot_json[i * 3 + 0]
+            )
+            rotations.append(rot_quat)
+
+        return positions, rotations
+
+
     def get_hand_pose(self, hand_idx, history_index=0):
         client_state = self.get_recent_client_state_by_history_index(
             history_index
@@ -204,19 +273,35 @@ class RemoteGuiInput:
             self._debug_line_render.pop_transform()
 
         for hand_idx in range(2):
-            hand_pos, hand_rot_quat = self.get_hand_pose(hand_idx)
-            if hand_pos is not None and hand_rot_quat is not None:
-                trans = mn.Matrix4.from_(hand_rot_quat.to_matrix(), hand_pos)
-                self._debug_line_render.push_transform(trans)
-                pointer_len = 0.5
-                self._debug_line_render.draw_transformed_line(
-                    mn.Vector3(0, 0, 0),
-                    mn.Vector3(0, 0, pointer_len),
-                    color0,
-                    color1,
-                )
+            # hand_pos, hand_rot_quat = self.get_hand_pose(hand_idx)
+            # if hand_pos is not None and hand_rot_quat is not None:
+            #     trans = mn.Matrix4.from_(hand_rot_quat.to_matrix(), hand_pos)
+            #     self._debug_line_render.push_transform(trans)
+            #     pointer_len = 0.5
+            #     self._debug_line_render.draw_transformed_line(
+            #         mn.Vector3(0, 0, 0),
+            #         mn.Vector3(0, 0, pointer_len),
+            #         color0,
+            #         color1,
+            #     )
+            #     self._debug_line_render.pop_transform()
 
-                self._debug_line_render.pop_transform()
+            art_hand_positions, art_hand_rotations = self.get_articulated_hand_pose(hand_idx)
+            if art_hand_positions is not None and art_hand_rotations is not None:
+                num_bones = len(art_hand_positions)
+                for i in range(num_bones):
+                    bone_pos = art_hand_positions[i]
+                    bone_rot_quat = art_hand_rotations[i]
+                    trans = mn.Matrix4.from_(bone_rot_quat.to_matrix(), bone_pos)
+                    self._debug_line_render.push_transform(trans)
+                    pointer_len = 0.02
+                    self._debug_line_render.draw_transformed_line(
+                        mn.Vector3(0, 0, 0),
+                        mn.Vector3(0, 0, pointer_len),
+                        color0,
+                        color1,
+                    )
+                    self._debug_line_render.pop_transform()                
 
     def _clean_history_by_connection_id(self, client_states):
         if not len(client_states):
