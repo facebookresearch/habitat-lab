46fafe1bc29300f097769e9047e8f719a5088b06
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2025-07-30 18:39:18 -0400

    Update xr reader application to visualize hand tracking.

--- examples/hitl/experimental/xr_reader/xr_reader.py
+++ examples/hitl/experimental/xr_reader/xr_reader.py
@@ -3,7 +3,9 @@
 # Copyright (c) Meta Platforms, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+from __future__ import annotations
 
+import math
 from typing import Optional
 
 import hydra
@@ -37,13 +39,56 @@ class AppStateXRReader(AppState):
         if self._app_service.gui_input.get_key_down(KeyCode.ESC):
             self._app_service.end_episode()
             post_sim_update_dict["application_exit"] = True
-        post_sim_update_dict["cam_transform"] = mn.Matrix4.identity_init()
 
+        # Draw debug visuals.
+        self._visualize_hands()
+        self._draw_debug_text()
+
+        # Update camera.
+        post_sim_update_dict["cam_transform"] = mn.Matrix4.identity_init()
+        state = self._app_service.remote_client_state
+        head_pos, head_rot = state.get_head_pose(0)
+        if head_pos is not None and head_rot is not None:
+            head_rot *= mn.Quaternion.rotation(
+                mn.Rad(math.pi), mn.Vector3(0.0, 1.0, 0.0)
+            )  # Sensors are backwards.
+            trans = mn.Matrix4.from_(head_rot.to_matrix(), head_pos)
+            post_sim_update_dict["cam_transform"] = trans
+
+    def _visualize_hands(self):
+        """Draw each hand joint."""
+        drawer = self._app_service.gui_drawer
+        state = self._app_service.remote_client_state
+        translations = []
+        rotations = []
+        hands = state.get_xr_input(0).hands
+        for hand_idx in range(len(hands)):
+            hand = hands[hand_idx]
+            d = 1.0 if hand.detected else 0.5
+            joint_color_0 = mn.Color4(0.3, 0.3, 1.0 * d, 1.0)
+            joint_color_1 = mn.Color4(0.3, 0.3, 1.0 * d, 0.0)
+
+            translations.extend(hand.joint_positions)
+            rotations.extend(hand.joint_rotations)
+
+            for t, r in zip(hand.joint_positions, hand.joint_rotations):
+                joint_pos = mn.Vector3(*t)
+                joint_rot = mn.Quaternion(mn.Vector3(r[1:4]), r[0])
+                drawer.draw_transformed_line(
+                    from_pos=joint_pos,
+                    to_pos=joint_pos
+                    + joint_rot.transform_vector(mn.Vector3(0.0, 0.0, 0.025)),
+                    from_color=joint_color_0,
+                    to_color=joint_color_1,
+                )
+
+    def _draw_debug_text(self):
+        """Display VR input state."""
+        state = self._app_service.remote_client_state
         left = self._xr_input.left_controller
         right = self._xr_input.right_controller
-        state = self._app_service.remote_client_state
 
-        def btn(button_list: list[XRButton]) -> str:
+        def txtbtn(button_list: set[XRButton]) -> str:
             button_names = [
                 button.name
                 for button in button_list
@@ -51,7 +96,7 @@ class AppStateXRReader(AppState):
             ]
             return f"[{', '.join(button_names)}]"
 
-        def pos(
+        def txtpos(
             legacy_tuple: Optional[tuple[mn.Vector3, mn.Quaternion]]
         ) -> str:
             if legacy_tuple is None:
@@ -63,11 +108,11 @@ class AppStateXRReader(AppState):
 
         text = f"""
         Left Controller:
-        - Position:     {pos(state.get_hand_pose(0, 0))}
-        - Keys held:    {btn(left._buttons_held)}
-        - Keys down:    {btn(left._buttons_down)}
-        - Keys up:      {btn(left._buttons_up)}
-        - Keys touched: {btn(left._buttons_touched)}
+        - Position:     {txtpos(state.get_hand_pose(0, 0))}
+        - Held:         {txtbtn(left._buttons_held)}
+        - Down:         {txtbtn(left._buttons_down)}
+        - Up:           {txtbtn(left._buttons_up)}
+        - Touched:      {txtbtn(left._buttons_touched)}
         - Hand trigger: {left.get_hand_trigger()}
         - Idx trigger:  {left.get_index_trigger()}
         - Thumbstick:   {left.get_thumbstick()}
@@ -80,11 +125,11 @@ class AppStateXRReader(AppState):
 
         text = f"""
         Right Controller:
-        - Position:     {pos(state.get_hand_pose(0, 1))}
-        - Keys held:    {btn(right._buttons_held)}
-        - Keys down:    {btn(right._buttons_down)}
-        - Keys up:      {btn(right._buttons_up)}
-        - Keys touched: {btn(right._buttons_touched)}
+        - Position:     {txtpos(state.get_hand_pose(0, 1))}
+        - Held:         {txtbtn(right._buttons_held)}
+        - Down:         {txtbtn(right._buttons_down)}
+        - Up:           {txtbtn(right._buttons_up)}
+        - Touched:      {txtbtn(right._buttons_touched)}
         - Hand trigger: {right.get_hand_trigger()}
         - Idx trigger:  {right.get_index_trigger()}
         - Thumbstick:   {right.get_thumbstick()}
@@ -97,7 +142,7 @@ class AppStateXRReader(AppState):
 
         text = f"""
         HMD:
-        - Position:     {pos(state.get_head_pose(0))}
+        - Position:     {txtpos(state.get_head_pose(0))}
         """
         self._app_service.text_drawer.add_text(
             text,
