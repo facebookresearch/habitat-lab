7aaf1d1bdb1f25a74c3ba63caf73e035cbf797d0
Alexander Clegg <alexanderwclegg@gmail.com>
2026-01-08 16:07:45 -0800

    [doc] - Add copyright info (#2217)
    * add some missing copyrights
    
    * update copyright header wording slightly
    
    * update more missing copyright headers and fix jupyter notebook copyright headers format issues

--- habitat-lab/habitat/tasks/rearrange/actions/articulated_agent_action.py
+++ habitat-lab/habitat/tasks/rearrange/actions/articulated_agent_action.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from habitat.core.embodied_task import SimulatorTaskAction
 from habitat.tasks.rearrange.rearrange_sim import RearrangeSim
 
